552e9aa9ae0ec67dbcad6f07d99651373c5e55b1
Alonso del Arte <alonso.delarte@gmail.com>
2021-03-14 23:57:53 -0400

    Starting sysops package

---
+++ src/sysops/EnvironmentVariableStore.java
@@ -0,0 +1,10 @@
+package sysops;
+
+public class EnvironmentVariableStore {
+
+    // STUB TO FAIL THE FIRST TEST
+    public String getVariable(String label) {
+        return "Sorry, not implemented yet";
+    }
+
+}
---
+++ test/sysops/EnvironmentVariableStoreTest.java
@@ -0,0 +1,13 @@
+package sysops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnvironmentVariableStoreTest {
+
+    @Test
+    void testGetVariable() {
+        fail("Haven't written test yet");
+    }
+}
