f31c83a2a64eee714f1a67beaba0342cff986af0
Alonso del Arte <alonso.delarte@gmail.com>
2021-07-06 17:40:42 -0400

    Caching enabled but no recency tracking yet

--- src/collections/LRUCache.java
+++ src/collections/LRUCache.java
@@ -2,7 +2,28 @@ package collections;
 
 public abstract class LRUCache<N, V> {
 
+    /**
+     * The minimum capacity for a cache. Obviously this should not be a negative
+     * number, and zero doesn't make sense either. A value of 1 would be
+     * pointless, since the cache would be constantly pushing items out. So
+     * perhaps 2 is the smallest value that makes sense. But I think 4 is the
+     * smallest value likely to be used with any frequency. I don't think the
+     * cache should be too large, however, though I'm not providing a maximum
+     * capacity constant.
+     */
     public static final int MINIMUM_CAPACITY = 4;
+
+    public static final int MAXIMUM_CAPACITY = 128;
+
+    private final Object[] names;
+
+    private final Object[] values;
+
+    private final int capacity;
+
+    private final int lastIndex;
+
+    private int nextUp = 0;
 
     /**
      * Creates a value for a given name. Ideally this function should only be
@@ -13,19 +34,57 @@ public abstract class LRUCache<N, V> {
      */
     protected abstract V create(N name);
 
+    private static int indexOf(Object obj, Object[] array, int endBound) {
+        boolean found = false;
+        int curr = 0;
+        while (!found && curr < endBound) {
+            found = obj.equals(array[curr]);
+            curr++;
+        }
+        if (found) {
+            return curr - 1;
+        } else {
+            return -1;
+        }
+    }
+
+    private int indexOf(V value) {
+        return Integer.MAX_VALUE;
+    }
+
     // TODO: Write a test for this
     boolean has(V value) {
         return false;
     }
 
-    // TODO: Write a test for this
+    private static void moveToFront(Object[] objects, int position) {
+        // TODO: Implement this
+    }
+
     public V retrieve(N name) {
-        return null;
+        V value;
+        int index = indexOf(name, this.names, this.nextUp);
+        if (index > -1) {
+            value = (V) this.values[index];
+        } else {
+            value = this.create(name);
+            this.names[this.nextUp] = name;
+            this.values[this.nextUp] = value;
+            this.nextUp++;
+            if (this.nextUp == this.capacity) {
+                this.nextUp--;
+            }
+        }
+        return value;
     }
 
     public LRUCache(int size) {
         // TODO: Throw exception if size < MINIMUM_CAPACITY
-        // TODO: Initialize queue
+        // TODO: Throw exception if size > MAXIMUM_CAPACITY
+        this.capacity = size;
+        this.names = new Object[this.capacity];
+        this.values = new Object[this.capacity];
+        this.lastIndex = this.capacity - 1;
     }
 
 }
--- test/collections/LRUCacheTest.java
+++ test/collections/LRUCacheTest.java
@@ -20,6 +20,30 @@ class LRUCacheTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void testRetrieve() {
+        System.out.println("retrieve");
+        LRUCacheImpl cache = new LRUCacheImpl(DEFAULT_SIZE);
+        String romanName = "^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})"
+                + "(I[XV]|V?I{0,3})$";
+        Pattern expected = cache.retrieve(romanName);
+        Pattern actual = cache.retrieve(romanName);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCacheRetainsValueWhileCapacityAvailable() {
+        LRUCacheImpl cache = new LRUCacheImpl(LRUCache.MINIMUM_CAPACITY);
+        String timeName = "^(?:\\d|[01]\\d|2[0-3]):[0-5]\\d$";
+        Pattern expected = cache.retrieve(timeName);
+        for (int i = 1; i < LRUCache.MINIMUM_CAPACITY; i++) {
+            String fillerName = "^" + i + "*$";
+            cache.retrieve(fillerName);
+        }
+        Pattern actual = cache.retrieve(timeName);
+        assertEquals(expected, actual);
+    }
+
     private static class LRUCacheImpl extends LRUCache<String, Pattern> {
 
         int createCallCount = 0;
