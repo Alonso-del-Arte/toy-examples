eb21b98dca47dd2acf2c58c8453b1094a29d3f83
Alonso del Arte <alonso.delarte@gmail.com>
2021-07-08 01:18:11 -0400

    Starting to flesh out set class

--- src/collections/ArrayBackedSet.java
+++ src/collections/ArrayBackedSet.java
@@ -1,7 +1,43 @@
 package collections;
 
-public class ArrayBackedSet {
+public class ArrayBackedSet<E> {
 
-    // TODO: Flesh out
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    private Object[] elements;
+
+    // TODO: Write test for this
+    public boolean add(E element) {
+        return false;
+    }
+
+    // TODO: Write test for this
+    public boolean contains(E element) {
+        return false;
+    }
+
+    // TODO: Write test for this
+    public boolean remove(E element) {
+        return false;
+    }
+
+    // TODO: Write test for this
+    public int size() {
+        return -1;
+    }
+
+    // TODO: Write test for this
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // TODO: Write test for this
+    public void clear() {
+        //
+    }
+
+    public ArrayBackedSet() {
+        this.elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
 
 }
