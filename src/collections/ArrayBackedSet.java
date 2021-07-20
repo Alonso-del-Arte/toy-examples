b8c12d21ee52c4841cc6f9420b6c4785a27ff271
Alonso del Arte <alonso.delarte@gmail.com>
2021-07-20 17:20:24 -0400

    Now passing size, duplicates, capacity tests

--- src/collections/ArrayBackedSet.java
+++ src/collections/ArrayBackedSet.java
@@ -8,7 +8,21 @@ public class ArrayBackedSet<E> {
 
     private int nextUp = 0;
 
+    private void expandCapacity() {
+        int largerSize = 3 * this.elements.length / 2;
+        Object[] largerArray = new Object[largerSize];
+        System.arraycopy(this.elements, 0, largerArray, 0,
+                this.elements.length);
+        this.elements = largerArray;
+    }
+
     public boolean add(E element) {
+        if (this.contains(element)) {
+            return false;
+        }
+        if (this.nextUp == this.elements.length) {
+            this.expandCapacity();
+        }
         this.elements[this.nextUp] = element;
         this.nextUp++;
         return true;
@@ -29,9 +43,8 @@ public class ArrayBackedSet<E> {
         return false;
     }
 
-    // TODO: Write test for this
     public int size() {
-        return -1;
+        return this.nextUp;
     }
 
     // TODO: Write test for this
