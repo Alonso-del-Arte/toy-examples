59728939d1d5f958506e047b39824b7f3291eb3a
Alonso del Arte <alonso.delarte@gmail.com>
2021-09-12 22:56:55 -0400

    Fleshing out stub

--- src/collections/immutable/ArrayBackedList.java
+++ src/collections/immutable/ArrayBackedList.java
@@ -2,5 +2,26 @@ package collections.immutable;
 
 import java.util.Iterator;
 
-public class ArrayBackedList {
+public class ArrayBackedList<E> implements Iterable<E> {
+
+    private static final int DEFAULT_CAPACITY_INITIAL = 16;
+
+    // TODO: Write test for this
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+
+        };
+    }
+
 }
