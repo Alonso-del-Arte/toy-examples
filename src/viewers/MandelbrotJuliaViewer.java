d99e2763feb5506f02add892d65cfc968ae80fea
Alonso del Arte <alonso.delarte@gmail.com>
2021-11-08 17:44:34 -0500

    Diagram dragging plus other improvements

--- src/viewers/MandelbrotJuliaViewer.java
+++ src/viewers/MandelbrotJuliaViewer.java
@@ -23,7 +23,6 @@ import numerics.ComplexNumber;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Event;
 import java.awt.Graphics;
@@ -33,11 +32,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 
 import javax.imageio.ImageIO;
 import javax.swing.JCheckBoxMenuItem;
@@ -54,36 +53,53 @@ import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 
 public final class MandelbrotJuliaViewer extends JPanel
-        implements ActionListener, MouseMotionListener {
+        implements ActionListener, MouseListener, MouseMotionListener {
 
     private static final boolean MAC_OS_FLAG = System.getProperty("os.name")
             .equals("Mac OS X");
 
     private static int maskCtrlCommand;
 
+    /**
+     * The complex number 0.0 + 0.0<i>i</i>.
+     */
     private static final ComplexNumber ZERO = new ComplexNumber(0.0, 0.0);
 
     private static final int BASELINE_READOUT_FIELD_COLUMNS = 20;
 
     private static final int DEFAULT_HORIZ_MAX = 1080;
 
-    private static final int DEFAULT_VERTIC_MAX = 640;
-
-    private int zeroCoordX;
-
-    private int zeroCoordY;
+    private static final int DEFAULT_VERTIC_MAX = 720;
 
     private int maxX, maxY;
 
-    private static final double DEFAULT_MANDEL_X_ADJUST = 0.75;
+    private int mousePressedX, mousePressedY;
 
-    private double mandelXAdjust = DEFAULT_MANDEL_X_ADJUST;
+    private static final ComplexNumber DEFAULT_MANDELBROT_TOP_LEFT_CORNER
+            = new ComplexNumber(-2.75, -1.25);
 
-    private double xAdjust;
+    private static final ComplexNumber DEFAULT_JULIA_TOP_LEFT_CORNER
+            = new ComplexNumber(-2.0, -1.25);
 
-    private static final int ITERATION_MAXIMUM = 256;
+    private ComplexNumber mandelbrotTopLeftCorner;
 
-    private JFrame frame;
+    private ComplexNumber juliaTopLeftCorner;
+
+    private ComplexNumber topLeftCorner;
+
+    private static final int DEFAULT_PIXELS_PER_UNIT_INTERVAL = 256;
+
+    private static final int MINIMUM_PIXELS_PER_UNIT_INTERVAL = 2;
+
+    private static final int MAXIMUM_PIXELS_PER_UNIT_INTERVAL = 131072;
+
+    private int pixelsPerUnitInterval;
+
+    private int prevMandelPxui;
+
+    private static final int DEFAULT_ITERATION_MAXIMUM = 256;
+
+    private int iterMax = DEFAULT_ITERATION_MAXIMUM;
 
     private ComplexNumber currPoint;
 
@@ -91,7 +107,11 @@ public final class MandelbrotJuliaViewer extends JPanel
 
     private boolean juliaFlag;
 
+    private JFrame frame;
+
     private JTextField readoutRe, readoutIm;
+
+    private JMenuItem zoomInMenuItem, zoomOutMenuItem;
 
     private JCheckBoxMenuItem toggleReadoutsEnabled;
 
@@ -99,16 +119,31 @@ public final class MandelbrotJuliaViewer extends JPanel
 
     private String prevSavePathname;
 
-    private ComplexNumber getNumber(int x, int y) {
-        int adjustX = x - this.zeroCoordX;
-        int adjustY = y - this.zeroCoordY;
-        double re = 2.0 * (double) adjustX / this.zeroCoordX - this.xAdjust;
-        double im = 1.25 * (double) adjustY / this.zeroCoordY;
+    public static final String PROGRAM_NAME = "Mandelbrot/Julia Set Viewer";
+
+    public static final String VERSION_ID = "Version 0.1";
+
+    public static final String COPYRIGHT_YEAR_AND_NAME = " 2021 Alonso del Arte";
+
+    public static final String ABOUT_BOX_MSG = PROGRAM_NAME + "\n" + VERSION_ID
+            + "\n\u00A9" + COPYRIGHT_YEAR_AND_NAME;
+
+    public static final String ABOUT_BOX_MSG_ASCII = PROGRAM_NAME + "\n"
+            + VERSION_ID + "\n(c)" + COPYRIGHT_YEAR_AND_NAME;
+
+    private ComplexNumber getRelativeNumber(int x, int y) {
+        double re = (double) x / this.pixelsPerUnitInterval;
+        double im = (double) y / this.pixelsPerUnitInterval;
         return new ComplexNumber(re, im);
     }
 
+    private ComplexNumber getNumber(int x, int y) {
+        ComplexNumber offset = this.getRelativeNumber(x, y);
+        return this.topLeftCorner.plus(offset);
+    }
+
     private Color chooseColor(int iterationCount) {
-        int adjustedCount = iterationCount % ITERATION_MAXIMUM;
+        int adjustedCount = iterationCount % DEFAULT_ITERATION_MAXIMUM;
         int r = (adjustedCount % 170) * 65536;
         int g = (adjustedCount % 85) * 256;
         int b = adjustedCount * 16;
@@ -119,7 +154,7 @@ public final class MandelbrotJuliaViewer extends JPanel
     private Color iteratePointJulia(ComplexNumber c) {
         int iterCount = 0;
         ComplexNumber z = c;
-        while (z.abs() < 2.0 && iterCount < ITERATION_MAXIMUM) {
+        while (z.abs() < 2.0 && iterCount < this.iterMax) {
             z = z.times(z).plus(this.juliaPoint);
             iterCount++;
         }
@@ -130,12 +165,11 @@ public final class MandelbrotJuliaViewer extends JPanel
         ComplexNumber curr;
         Color color;
         for (int x = 0; x < this.maxX; x++) {
-            for (int y = 0; y < this.zeroCoordY; y++) {
+            for (int y = 0; y < this.maxY; y++) {
                 curr = this.getNumber(x, y);
                 color = this.iteratePointJulia(curr);
                 g.setColor(color);
                 g.drawRect(x, y, 1, 1);
-                g.drawRect(this.maxX - x, this.maxY - y, 1, 1);
             }
         }
     }
@@ -143,7 +177,7 @@ public final class MandelbrotJuliaViewer extends JPanel
     private Color iteratePointMandel(ComplexNumber c) {
         int iterCount = 0;
         ComplexNumber z = c;
-        while (z.abs() < 2.0 && iterCount < ITERATION_MAXIMUM) {
+        while (z.abs() < 2.0 && iterCount < this.iterMax) {
             z = z.times(z).plus(c);
             iterCount++;
         }
@@ -154,12 +188,11 @@ public final class MandelbrotJuliaViewer extends JPanel
         ComplexNumber curr;
         Color color;
         for (int x = 0; x < this.maxX; x++) {
-            for (int y = 0; y < this.zeroCoordY; y++) {
+            for (int y = 0; y < this.maxY; y++) {
                 curr = this.getNumber(x, y);
                 color = this.iteratePointMandel(curr);
                 g.setColor(color);
                 g.drawRect(x, y, 1, 1);
-                g.drawRect(x, this.maxY - y, 1, 1);
             }
         }
     }
@@ -183,9 +216,11 @@ public final class MandelbrotJuliaViewer extends JPanel
         if (this.juliaFlag) {
             String pointStr = this.juliaPoint.toString().replace("\u2212", "n")
                     .replace(".", "d").replace("+", "p");
-            suggestedFilename = "Julia Set " + pointStr + ".png";
+            suggestedFilename = "Julia Set " + pointStr + " pxui"
+                    + this.pixelsPerUnitInterval + ".png";
         } else {
-            suggestedFilename = "Mandelbrot Set.png";
+            suggestedFilename = "Mandelbrot Set pxui"
+                    + this.pixelsPerUnitInterval + ".png";
         }
         File diagramFile = new File(suggestedFilename);
         FileChooserWithOverwriteGuard fileChooser
@@ -248,12 +283,56 @@ public final class MandelbrotJuliaViewer extends JPanel
         if (this.juliaFlag) {
             String text = "Julia set for " + this.juliaPoint.toString();
             this.frame.setTitle(text);
-            this.xAdjust = 0.0;
+            this.prevMandelPxui = this.pixelsPerUnitInterval;
+            this.pixelsPerUnitInterval = DEFAULT_PIXELS_PER_UNIT_INTERVAL;
+            this.mandelbrotTopLeftCorner = this.topLeftCorner;
+            this.topLeftCorner = this.juliaTopLeftCorner;
         } else {
             this.frame.setTitle("Mandelbrot set");
-            this.xAdjust = this.mandelXAdjust;
+            this.pixelsPerUnitInterval = this.prevMandelPxui;
+            this.topLeftCorner = this.juliaTopLeftCorner;
+            this.topLeftCorner = this.mandelbrotTopLeftCorner;
         }
+        this.checkZoomMenuEnablements();
         this.repaint();
+        this.checkIterationMaximum();
+    }
+
+    private void checkZoomMenuEnablements() {
+        boolean zoomInAllowed
+                = this.pixelsPerUnitInterval < MAXIMUM_PIXELS_PER_UNIT_INTERVAL;
+        this.zoomInMenuItem.setEnabled(zoomInAllowed);
+        boolean zoomOutAllowed
+                = this.pixelsPerUnitInterval > MINIMUM_PIXELS_PER_UNIT_INTERVAL;
+        this.zoomOutMenuItem.setEnabled(zoomOutAllowed);
+    }
+
+    private void checkIterationMaximum() {
+        if (this.pixelsPerUnitInterval < 32768) {
+            this.iterMax = DEFAULT_ITERATION_MAXIMUM;
+        } else {
+            this.iterMax = 2048;
+        }
+    }
+
+    private void zoomIn() {
+        int replacementX = this.maxX / 4;
+        int replacementY = this.maxY / 4;
+        this.topLeftCorner = this.getNumber(replacementX, replacementY);
+        this.pixelsPerUnitInterval *= 2;
+        this.repaint();
+        this.checkZoomMenuEnablements();
+        this.checkIterationMaximum();
+    }
+
+    private void zoomOut() {
+        int replacementX = this.maxX / 4;
+        int replacementY = this.maxY / 4;
+        this.topLeftCorner = this.getNumber(replacementX, replacementY);
+        this.pixelsPerUnitInterval /= 2;
+        this.repaint();
+        this.checkZoomMenuEnablements();
+        this.checkIterationMaximum();
     }
 
     private void setToggleReadoutsEnabled() {
@@ -266,11 +345,9 @@ public final class MandelbrotJuliaViewer extends JPanel
 
     private void showAboutBox() {
         String title = "About";
-        String msg = "Mandelbrot/Julia Set Viewer\nVersion 0.1\n"
-                + "\u00A9 2021 Alonso del Arte";
-        JOptionPane.showMessageDialog(this.frame, msg, title,
+        JOptionPane.showMessageDialog(this.frame, ABOUT_BOX_MSG, title,
                 JOptionPane.PLAIN_MESSAGE);
-        System.out.println(msg);
+        System.out.println(ABOUT_BOX_MSG_ASCII);
     }
 
     @Override
@@ -296,12 +373,10 @@ public final class MandelbrotJuliaViewer extends JPanel
                 this.toggleJuliaFlag();
                 break;
             case "zoomIn":
-//                this.zoomIn();
-                System.err.println("Sorry, zoom in not available yet");
+                this.zoomIn();
                 break;
             case "zoomOut":
-//                this.zoomOut();
-                System.err.println("Sorry, zoom out not available yet");
+                this.zoomOut();
                 break;
             case "decreaseZoomStep":
 //                this.decreaseZoomStep();
@@ -320,6 +395,41 @@ public final class MandelbrotJuliaViewer extends JPanel
                 break;
             default:
                 System.err.println("Command " + cmd + " not recognized");
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        //
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        //
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        //
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        this.mousePressedX = me.getX();
+        this.mousePressedY = me.getY();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        int mouseReleasedX = me.getX();
+        int mouseReleasedY = me.getY();
+        if (this.mousePressedX != mouseReleasedX
+                || this.mousePressedY != mouseReleasedY) {
+            int x = this.mousePressedX - mouseReleasedX;
+            int y = this.mousePressedY - mouseReleasedY;
+            ComplexNumber offset = this.getRelativeNumber(x, y);
+            this.topLeftCorner = this.topLeftCorner.plus(offset);
+            this.repaint();
         }
     }
 
@@ -407,6 +517,35 @@ public final class MandelbrotJuliaViewer extends JPanel
         JMenuItem menuItem = this.makeMenuItem("Toggle Mandelbrot/Julia",
                 accDescr, "mjToggle", accelerator);
         menu.add(menuItem);
+        menu.addSeparator();
+        accDescr = "Zoom in, by increasing pixels per unit interval";
+        if (MAC_OS_FLAG) {
+            accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS,
+                    Event.SHIFT_MASK);
+        } else {
+            accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_ADD,
+                    Event.CTRL_MASK);
+        }
+        menuItem = this.makeMenuItem("Zoom in", accDescr, "zoomIn",
+                accelerator);
+        this.zoomInMenuItem = menu.add(menuItem);
+        if (this.pixelsPerUnitInterval >= MAXIMUM_PIXELS_PER_UNIT_INTERVAL) {
+            this.zoomInMenuItem.setEnabled(false);
+        }
+        accDescr = "Zoom out, by decreasing pixels per unit interval";
+        if (MAC_OS_FLAG) {
+            accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, 0);
+        } else {
+            accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_SUBTRACT,
+                    Event.CTRL_MASK);
+        }
+        menuItem = this.makeMenuItem("Zoom out", accDescr, "zoomOut",
+                accelerator);
+        this.zoomOutMenuItem = menu.add(menuItem);
+        if (this.pixelsPerUnitInterval <= MINIMUM_PIXELS_PER_UNIT_INTERVAL) {
+            this.zoomOutMenuItem.setEnabled(false);
+        }
+        menu.addSeparator();
         this.toggleReadoutsEnabled = new JCheckBoxMenuItem("Update readouts",
                 true);
         this.toggleReadoutsEnabled.getAccessibleContext()
@@ -469,27 +608,31 @@ public final class MandelbrotJuliaViewer extends JPanel
         } else {
             maskCtrlCommand = Event.CTRL_MASK;
         }
-        String text = "Mandelbrot set";
-        this.xAdjust = this.mandelXAdjust;
-        if (this.juliaFlag) {
-            text = "Julia set for " + this.juliaPoint.toString();
-            this.xAdjust = 0.0;
-        }
-        this.frame = new JFrame(text);
-        this.frame.setJMenuBar(this.setUpMenuBar());
         this.setBackground(Color.BLACK);
         Dimension dimension = new Dimension(DEFAULT_HORIZ_MAX,
                 DEFAULT_VERTIC_MAX);
         this.setPreferredSize(dimension);
         this.maxX = DEFAULT_HORIZ_MAX;
         this.maxY = DEFAULT_VERTIC_MAX;
-        this.zeroCoordX = this.maxX / 2;
-        this.zeroCoordY = this.maxY / 2;
+        this.mandelbrotTopLeftCorner = DEFAULT_MANDELBROT_TOP_LEFT_CORNER;
+        this.juliaTopLeftCorner = DEFAULT_JULIA_TOP_LEFT_CORNER;
+        this.pixelsPerUnitInterval = DEFAULT_PIXELS_PER_UNIT_INTERVAL;
+        this.prevMandelPxui = this.pixelsPerUnitInterval;
+        String text = "Mandelbrot set";
+        if (this.juliaFlag) {
+            text = "Julia set for " + this.juliaPoint.toString();
+            this.topLeftCorner = this.juliaTopLeftCorner;
+        } else {
+            this.topLeftCorner = this.mandelbrotTopLeftCorner;
+        }
+        this.frame = new JFrame(text);
+        this.frame.setJMenuBar(this.setUpMenuBar());
         this.frame.add(this, BorderLayout.CENTER);
         this.frame.add(this.setUpReadOuts(), BorderLayout.PAGE_END);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.pack();
         this.frame.setVisible(true);
+        this.addMouseListener(this);
         this.addMouseMotionListener(this);
     }
 
