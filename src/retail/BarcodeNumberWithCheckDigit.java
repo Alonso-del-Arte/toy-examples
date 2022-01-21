a056228610cdfe1da9049dfd0a3fd9239d20c639
Alonso del Arte <alonso.delarte@gmail.com>
2022-01-21 17:07:16 -0500

    Passing first test

--- src/retail/BarcodeNumberWithCheckDigit.java
+++ src/retail/BarcodeNumberWithCheckDigit.java
@@ -10,9 +10,20 @@ public abstract class BarcodeNumberWithCheckDigit implements Serializable {
 
     final byte checkDigit;
 
-    // TODO: Write tests for this
     static byte calculateCheckDigit(long num) {
-        return 0;
+        switch ((int) (num % 10)) {//0, 7, 4, 1, 8, 5, 2, 9, 6, 3
+            case 0: return 0;
+            case 1: return 7;
+            case 2: return 4;
+            case 3: return 1;
+            case 4: return 8;
+            case 5: return 5;
+            case 6: return 2;
+            case 7: return 9;
+            case 8: return 6;
+            case 9: return 3;
+            default: return -1;
+        }
     }
 
     BarcodeNumberWithCheckDigit(long num) {
