4a99587b086eeeca51ea90a681cc58991bc1a122
Alonso del Arte <alonso.delarte@gmail.com>
2022-03-12 01:32:41 -0500

    Tests for hashCode( ), getNumericApproximation( )

--- test/fractions/FractionTest.java
+++ test/fractions/FractionTest.java
@@ -2,7 +2,7 @@ package fractions;
 
 import calculators.IntegerMath;
 
-import java.util.Random;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +29,20 @@ class FractionTest {
         Fraction fraction = new Fraction(numerator, expected);
         long actual = fraction.getDenominator();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetNumericApproximation() {
+        System.out.println("getNumericApproximation");
+        int denominator = RANDOM.nextInt(512) + 8;
+        int numerator = RANDOM.nextInt(2 * denominator) - denominator;
+        Fraction fraction = new Fraction(numerator, denominator);
+        double expected = ((double) numerator) / denominator;
+        double actual = fraction.getNumericApproximation();
+        double delta = 0.00000001;
+        String msg = "Numeric approximation of " + fraction
+                + " should be roughly " + expected;
+        assertEquals(expected, actual, delta, msg);
     }
 
     @Test
@@ -114,7 +128,7 @@ class FractionTest {
         assertNotEquals(fractionA, fractionB);
     }
 
-//    @Test
+    @Test
     void testEquals() {
         System.out.println("equals");
         int numerator = RANDOM.nextInt(1024) + 16;
@@ -124,6 +138,42 @@ class FractionTest {
         Fraction sameFraction = new Fraction(multiplier * numerator,
                 multiplier * denominator);
         assertEquals(someFraction, sameFraction);
+    }
+
+    @Test
+    void testHashCode() {
+        System.out.println("hashCode");
+        Set<Fraction> fractions = new HashSet<>();
+        Set<Integer> hashes = new HashSet<>();
+        for (int numer = -16; numer < 16; numer++) {
+            for (int denom = 1; denom < 16; denom++) {
+                Fraction fraction = new Fraction(numer, denom);
+                fractions.add(fraction);
+                hashes.add(fraction.hashCode());
+            }
+        }
+        int expected = fractions.size();
+        int actual = hashes.size();
+        String msg = "Given " + expected + " distinct fractions, " + expected
+                + " distinct hash codes are expected";
+        assertEquals(expected, actual, msg);
+    }
+
+    @Test
+    void testCompareTo() {
+        List<Fraction> expected = new ArrayList<>();
+        for (int numer = -63; numer < 64; numer++) {
+            expected.add(new Fraction(numer, 64));
+        }
+        List<Fraction> actual = new ArrayList<>();
+        for (int denom = 64; denom > 0; denom /= 2) {
+            int start = -denom + 1;
+            for (int numer = start; numer < denom; numer += 2) {
+                actual.add(new Fraction(numer, denom));
+            }
+        }
+        Collections.sort(actual);
+        assertEquals(expected, actual);
     }
 
     @Test
