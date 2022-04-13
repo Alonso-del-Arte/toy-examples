a0e50e26edc9411e03e23964fca37d6a222612a5
Alonso del Arte <alonso.delarte@gmail.com>
2022-04-12 23:31:26 -0400

    Getting started with tests

--- src/arithmetic/CollatzFunctions.java
+++ src/arithmetic/CollatzFunctions.java
@@ -3,4 +3,14 @@ package arithmetic;
 import java.util.function.IntUnaryOperator;
 
 public class CollatzFunctions {
+
+    // TODO: Write tests for this
+    public static final IntUnaryOperator CLASSIC = (n) -> 3 * n + 1;
+
+    // TODO: Write tests for this
+    public static final IntUnaryOperator THREE_X_MINUS_1 = (n) -> 4 * n;
+
+    // TODO: Write tests for this
+    public static final IntUnaryOperator FIVE_X_PLUS_1 = (n) -> 4 * n;
+
 }
--- test/arithmetic/CollatzFunctionsTest.java
+++ test/arithmetic/CollatzFunctionsTest.java
@@ -1,7 +1,34 @@
 package arithmetic;
 
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CollatzFunctionsTest {
+
+    private static final Random RANDOM = new Random();
+
+    @Test
+    void testClassicOdd() {
+        int start = 2 * RANDOM.nextInt(1024) + 1;
+        int end = start + 2 * RANDOM.nextInt(512) + 1;
+        for (int n = start; n < end; n += 2) {
+            int expected = 3 * n + 1;
+            int actual = CollatzFunctions.CLASSIC.applyAsInt(n);
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    void testClassicEven() {
+        int start = 2 * RANDOM.nextInt(1024);
+        int end = start + 2 * RANDOM.nextInt(512);
+        for (int n = start; n < end; n += 2) {
+            int expected = n / 2;
+            int actual = CollatzFunctions.CLASSIC.applyAsInt(n);
+            assertEquals(expected, actual);
+        }
+    }
 
 }
