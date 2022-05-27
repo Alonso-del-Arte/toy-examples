d0d2cb3d1c13d08ad637e25b1417f738fcfb8aec
Alonso del Arte <alonso.delarte@gmail.com>
2022-05-26 22:55:00 -0400

    New class to hold currency pairs

---
+++ src/currency/CurrencyPair.java
@@ -0,0 +1,19 @@
+package currency;
+
+import java.util.Currency;
+
+public class CurrencyPair {
+
+    // TODO: Write tests for this
+    public Currency getFrom() {
+        return null;
+    }
+
+    // TODO: Write tests for this
+    public Currency getTo() {
+        return null;
+    }
+
+    public CurrencyPair(Currency from, Currency to) {}
+
+}
