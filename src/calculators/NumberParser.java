14b31c070fe90c407bb6552ca5fddb3a43689fdd
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-14 22:59:48 -0400

    Starting work on NumberParser

---
+++ src/calculators/NumberParser.java
@@ -0,0 +1,10 @@
+package calculators;
+
+public class NumberParser {
+
+    // TODO: Write tests for this
+    public static int interpret(String s) {
+        return Integer.MIN_VALUE;
+    }
+
+}
---
+++ test/calculators/NumberParserTest.java
@@ -0,0 +1,13 @@
+package calculators;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberParserTest {
+
+    @Test
+    void testInterpretZeroToNine() {
+        fail("Haven't written test yet");
+    }
+
+}
