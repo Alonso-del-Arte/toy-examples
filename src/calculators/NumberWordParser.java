69173a6597f1a3ba2e8f42f0fc0768a1884895cd
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-14 23:28:20 -0400

    More prep

--- src/calculators/NumberWordParser.java
+++ src/calculators/NumberWordParser.java
@@ -1,10 +1,26 @@
 package calculators;
 
-public class NumberParser {
+import java.util.Locale;
+
+public class NumberWordParser {
+
+    private static int interpretEnglishNumberWord(String s) {
+        return Integer.MIN_VALUE;
+    }
 
     // TODO: Write tests for this
     public static int interpret(String s) {
-        return Integer.MIN_VALUE;
+        return interpretEnglishNumberWord(s);
+    }
+
+    public static int interpret(String s, Locale locale) {
+        if (locale.getISO3Language().equals("eng")) {
+            return interpretEnglishNumberWord(s);
+        } else {
+            String excMsg = "Language " + locale.getDisplayLanguage()
+                    + " is not supported yet";
+            throw new UnsupportedOperationException(excMsg);
+        }
     }
 
 }
--- test/calculators/NumberWordParserTest.java
+++ test/calculators/NumberWordParserTest.java
@@ -3,10 +3,17 @@ package calculators;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class NumberParserTest {
+/**
+ * Tests of the NumberWordParser class. For now, all these tests pertain to
+ * parsing English number words.
+ * @author Alonso del Arte
+ */
+class NumberWordParserTest {
 
     @Test
     void testInterpretZeroToNine() {
+        String[] numberWords = {"zero", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine"};
         fail("Haven't written test yet");
     }
 
