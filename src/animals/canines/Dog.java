35850a4d00d2b270626969b5f402849c2b5e4a12
Alonso del Arte <alonso.delarte@gmail.com>
2022-09-09 22:45:40 -0400

    New interfaces

--- src/animals/canines/Dog.java
+++ src/animals/canines/Dog.java
@@ -2,9 +2,17 @@ package animals.canines;
 
 public class Dog extends Canine {
 
+    public final String species() {
+        return "Canis lupus";
+    }
+
     @Override
     public String getTaxonomicClassification() {
         return "Canis familiaris";
+    }
+
+    public void bark() {
+        System.out.println("Arf! Arf!");
     }
 
 }
