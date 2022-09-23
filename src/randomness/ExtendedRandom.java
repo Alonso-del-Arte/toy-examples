1ffb5213808034bf9e9795ba6ab1a074f55a819b
Alonso del Arte <alonso.delarte@gmail.com>
2022-09-23 15:01:43 -0400

    Filled in nextFraction( )

--- src/randomness/ExtendedRandom.java
+++ src/randomness/ExtendedRandom.java
@@ -2,14 +2,27 @@ package randomness;
 
 import currency.CurrencyAmount;
 import fractions.Fraction;
+
+import java.util.Random;
+
 import numerics.ComplexNumber;
 import numerics.RomanNumeralsNumber;
 
 public class ExtendedRandom {
 
-    // TODO: Write tests for this
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Gives a pseudorandomly chosen fraction. May be positive or negative, or
+     * it could even be 0.
+     * @return A fraction with a pseudorandom signed 32-bit integer for the
+     * numerator and a denominator of at least 1 and at most
+     * <code>Integer.MAX_VALUE</code>.
+     */
     public static Fraction nextFraction() {
-        return new Fraction(0);
+        int numer = RANDOM.nextInt();
+        int denom = RANDOM.nextInt(Integer.MAX_VALUE) + 1;
+        return new Fraction(numer, denom);
     }
 
     // TODO: Write tests for this
--- test/randomness/ExtendedRandomTest.java
+++ test/randomness/ExtendedRandomTest.java
@@ -1,7 +1,33 @@
 package randomness;
 
+import currency.CurrencyAmount;
+import fractions.Fraction;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import numerics.ComplexNumber;
+import numerics.RomanNumeralsNumber;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExtendedRandomTest {
+
+    @Test
+    void testNextFraction() {
+        System.out.println("nextFraction");
+        int capacity = 1000;
+        Set<Fraction> fractions = new HashSet<>(capacity);
+        for (int i = 0; i < capacity; i++) {
+            fractions.add(ExtendedRandom.nextFraction());
+        }
+        int expected = 3 * capacity / 4;
+        int actual = fractions.size();
+        String msg = "Expected at least " + expected
+                + " distinct fractions out of " + capacity + ", got " + actual;
+        System.out.println(msg);
+        assert actual >= expected : msg;
+    }
 
 }
