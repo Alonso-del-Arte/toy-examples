0fa0a55270980a49a3aa6c04b4b23ef63d531978
Alonso del Arte <alonso.delarte@gmail.com>
2022-09-23 00:50:30 -0400

    Couple of stubs

--- src/randomness/ExtendedRandom.java
+++ src/randomness/ExtendedRandom.java
@@ -1,4 +1,20 @@
 package randomness;
 
+import currency.CurrencyAmount;
+import fractions.Fraction;
+import numerics.ComplexNumber;
+import numerics.RomanNumeralsNumber;
+
 public class ExtendedRandom {
+
+    // TODO: Write tests for this
+    public static Fraction nextFraction() {
+        return new Fraction(0);
+    }
+
+    // TODO: Write tests for this
+    public static ComplexNumber nextComplex() {
+        return new ComplexNumber(0.0, 0.0);
+    }
+
 }
