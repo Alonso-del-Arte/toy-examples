cd71c122ffafb78f5cd23133fee74c1b5d51f946
Alonso del Arte <alonso.delarte@gmail.com>
2022-10-10 13:53:03 -0400

    Starting on equals( ) tests

--- test/collections/immutable/ArrayBackedListTest.java
+++ test/collections/immutable/ArrayBackedListTest.java
@@ -1,6 +1,7 @@
 package collections.immutable;
 
 import java.math.BigInteger;
+import java.sql.ResultSet;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +20,43 @@ class ArrayBackedListTest {
             assertEquals(expected, actual);
             list = list.add(expected);
         }
+    }
+
+    @Test
+    void testContains() {
+        System.out.println("contains");
+        int firstElem = ExtendedRandom.nextInt(Short.MAX_VALUE);
+        int lastElem = firstElem + ExtendedRandom.nextInt(128) + 32;
+        ArrayBackedList<Integer> list = new ArrayBackedList<>();
+        for (int elem = firstElem; elem <= lastElem; elem++) {
+            String beforeMsg = "List should not contain " + elem
+                    + " before it's added to the list";
+            assert !list.contains(elem) : beforeMsg;
+            list.add(elem);
+            String afterMsg = "List should contain " + elem
+                    + " after it's added to the list";
+            assert list.contains(elem) : afterMsg;
+        }
+    }
+
+    @Test
+    void testReferentialEquality() {
+        ArrayBackedList<ResultSet> someList = new ArrayBackedList<>();
+        assertEquals(someList, someList);
+    }
+
+    @Test
+    void testConstructorRejectsNullElement() {
+        String msg = "Constructor should reject null elements";
+        Throwable t = assertThrows(NullPointerException.class, () -> {
+            ArrayBackedList<ResultSet> list
+                    = new ArrayBackedList<>(null, null, null);
+            System.out.println("Should not have been able to create " + list
+                    + " with null elements");
+        }, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        System.out.println("\"" + excMsg + "\"");
     }
 
 }
