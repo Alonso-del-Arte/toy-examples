6bc6d3769a91abdc19ab099db5bf5a3bede4b3e6
Alonso del Arte <alonso.delarte@gmail.com>
2022-10-10 13:45:11 -0400

    equals( ), hashCode( ) stub

--- src/collections/immutable/ArrayBackedList.java
+++ src/collections/immutable/ArrayBackedList.java
@@ -6,8 +6,11 @@ public class ArrayBackedList<E> implements Iterable<E> {
 
     int count = 0;
 
+    private final Object[] elements = new Object[256];
+
     // TODO: Write test for this
     public ArrayBackedList<E> add(E element) {
+        this.elements[this.count] = element;
         this.count++;
         return this;
     }
@@ -23,8 +26,18 @@ public class ArrayBackedList<E> implements Iterable<E> {
     }
 
     // TODO: Write test for this
+    public int indexOf(E element) {
+        return Integer.MAX_VALUE;
+    }
+
     public boolean contains(E element) {
-        return false;
+        boolean found = false;
+        int index = 0;
+        while (!found && index < this.elements.length) {
+            found = element.equals(this.elements[index]);
+            index++;
+        }
+        return found;
     }
 
     public int size() {
@@ -54,12 +67,29 @@ public class ArrayBackedList<E> implements Iterable<E> {
         };
     }
 
+    // TODO: Override toString()
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
     public ArrayBackedList() {
         // TODO: Write tests for this
     }
 
     // TODO: Determine if this needs SafeVarargs annotation
     public ArrayBackedList(E... elements) {
+        for (E element : elements) {
+            if (element == null) {
+                throw new NullPointerException("Element should not be null");
+            }
+        }
         // TODO: Write tests for this
     }
 
