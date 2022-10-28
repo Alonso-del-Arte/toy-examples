213a69bd6a30def3c7b37d18e451b98ccf25ac00
Alonso del Arte <alonso.delarte@gmail.com>
2022-10-28 15:43:31 -0400

    All available locales test

--- test/textops/LocaleParserTest.java
+++ test/textops/LocaleParserTest.java
@@ -14,4 +14,14 @@ class LocaleParserTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void testParse() {
+        System.out.println("parse");
+        Locale[] locales = Locale.getAvailableLocales();
+        for (Locale expected : locales) {
+            Locale actual = LocaleParser.parse(expected.toString());
+            assertEquals(expected, actual);
+        }
+    }
+
 }
