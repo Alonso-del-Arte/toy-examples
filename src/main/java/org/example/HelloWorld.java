8cc67dc1a42dd56536a5bf56526c833238b924b1
Alonso del Arte <galberto313@gmail.com>
2022-10-30 23:32:38 -0400

    passing the three tests

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -8,10 +8,31 @@ import textops.LocaleParser;
 public class HelloWorld {
 
     static String greeting(Locale locale) {
-        if (locale.toString().equals("es_US")) {
-            return "\u00A1Hola, mundo!";
+//        if (locale.toString().equals("es_US")) {
+//            return "\u00A1Hola, mundo!";
+//        }
+        switch (locale.getISO3Language()) {
+            case "deu":
+                return "Hallo Welt!";
+            case "eng":
+                return "Hello, world!";
+            case "fra":
+                return "Bonjour le monde!";
+            case "ita":
+                return "Ciao mondo!";
+            case "jpn":
+                return "\u3053\u3093\u306B\u3061\u306F\u4E16\u754C\uFF01";
+            case "kor":
+                return "\uC548\uB155\uD558\uC138\uC694, "
+                        + "\uC138\uACC4\uC785\uB2C8\uB2E4!";
+            case "spa":
+                return "\u00A1Hola, mundo!";
+            case "zho":
+                return "\u4F60\u597D\u4E16\u754C\uFF01";
+            default:
+                return "Greeting for " + locale.getDisplayLanguage()
+                        + " language not on file yet";
         }
-        return "Hello, world!";
     }
 
     public static void main(String[] args) {
