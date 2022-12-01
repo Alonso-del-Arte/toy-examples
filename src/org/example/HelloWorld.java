cd6e5a81fdeaf9be82486245df92ac02343f7483
Alonso del Arte <alonso.delarte@gmail.com>
2022-12-01 10:21:31 -0500

    Javadoc

--- src/org/example/HelloWorld.java
+++ src/org/example/HelloWorld.java
@@ -7,6 +7,18 @@ import textops.LocaleParser;
 
 public class HelloWorld {
 
+    /**
+     * Gives the "Hello, world" greeting in a language appropriate for the given
+     * locale. All the greetings are "hard-coded."
+     * @param locale The locale. For example, <code>Locale.CANADA_FRENCH</code>.
+     * @return The greeting. For example, "Bonjour le monde!" If a greeting is
+     * not available here, this will instead return a message saying so.
+     * @author Tom&aacute;&scaron; Ko&#345;&oacute;nek
+     * @author Irem Ugurlu had an earlier version which relied on classes from
+     * <code>com.google.cloud.translate</code>. It worked fine, from what I
+     * could tell, but these are supposed to be toy examples without
+     * dependencies.
+     */
     static String greeting(Locale locale) {
         switch (locale.getISO3Language()) {
             case "deu":
