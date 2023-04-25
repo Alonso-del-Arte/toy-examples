5d4bc744b9881f4a00362357d5252a2eb494cbbe
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-24 22:58:46 -0400

    Correction

--- test/calculators/ComplexMathTest.java
+++ test/calculators/ComplexMathTest.java
@@ -13,7 +13,7 @@ class ComplexMathTest {
     void testSqrtOfNegativeOne() {
         System.out.println("sqrt");
         ComplexNumber negOne = new ComplexNumber(-1.0, 0.0);
-        ComplexNumber expected = new ComplexNumber(0.0, -1.0);
+        ComplexNumber expected = new ComplexNumber(0.0, 1.0);
         ComplexNumber actual = ComplexMath.sqrt(negOne);
         String msgPart = " part of sqrt(-1) expected to be ";
         String msgRe = "Real" + msgPart + expected.getRealPart();
