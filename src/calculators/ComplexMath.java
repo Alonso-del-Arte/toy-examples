f46c84d2a958df447449cecf8dc954963b5d6bfa
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-09 18:41:06 -0400

    Passing both tests

--- src/calculators/ComplexMath.java
+++ src/calculators/ComplexMath.java
@@ -7,11 +7,13 @@ public class ComplexMath {
     public static ComplexNumber sqrt(ComplexNumber z) {
         double a = z.getRealPart();
         double b = z.getImagPart();
+        if (b == 0) {
+            return new ComplexNumber(0.0, 1.0);
+        }
         double norm = z.abs();
         double re = Math.sqrt((norm + a) / 2);
         double im = Math.sqrt((norm - a) / 2) * (b / Math.abs(b));
         return new ComplexNumber(re, im);
-//        return new ComplexNumber(0.0, 1.0);
     }
 
 }
