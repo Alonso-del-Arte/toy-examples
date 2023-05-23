954a803a861924565e180e29a346f579adf3901c
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-23 01:11:59 -0400

    Failing plus( ) test

--- test/calculators/ToyCalculatorTest.java
+++ test/calculators/ToyCalculatorTest.java
@@ -3,6 +3,8 @@ package calculators;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import randomness.ExtendedRandom;
+
 class ToyCalculatorTest {
 
     @Test
@@ -10,6 +12,21 @@ class ToyCalculatorTest {
         int expected = 2;
         int actual = ToyCalculator.plus(1, 1);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testPlus() {
+        System.out.println("plus");
+        int addendA = ExtendedRandom.nextInt(1024) - 512;
+        int addendB = ExtendedRandom.nextInt(1024) - 512;
+        int expected = addendA + addendB;
+        int actual = ToyCalculator.plus(addendA, addendB);
+        String msg = "Calculating " + addendA + " + " + addendB;
+        assertEquals(expected, actual, msg);
+    }
+
+    void testDivisionByZero() {
+        fail();
     }
 
 }
