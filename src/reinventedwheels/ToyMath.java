7e109d8b6cc6e4f2af62d4845c3f25908e00f3ef
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-27 17:26:46 -0400

    Passing random( ) gives enough distinct numbers test

--- src/reinventedwheels/ToyMath.java
+++ src/reinventedwheels/ToyMath.java
@@ -1,6 +1,18 @@
 package reinventedwheels;
 
+/**
+ * The idea here is to reproduce <code>java.lang.Math</code> entirely "in
+ * software," that is to say, without "native methods." This is of course
+ * completely unnecessary for real world projects. The value here is purely
+ * educational.
+ * @author Alonso del Arte
+ */
 class ToyMath {
+
+    private static final long FLOATING_POINT_ALMOST_ONE_BIT_PATTERN
+            = 0x3FEFFFFFFFFFFFFFL;
+
+    private static long previousBitPattern = Double.doubleToLongBits(0.5);
 
     // TODO: Write tests for this
     static double floor(double x) {
@@ -17,9 +29,12 @@ class ToyMath {
         return Double.MIN_VALUE;
     }
 
-    // TODO: Write tests for this
     static double random() {
-        return 0.5;
+        long millis = System.currentTimeMillis();
+        long bitPattern = (millis ^ (previousBitPattern * 37))
+                & FLOATING_POINT_ALMOST_ONE_BIT_PATTERN;
+        previousBitPattern = bitPattern;
+        return Double.longBitsToDouble(bitPattern);
     }
 
 }
