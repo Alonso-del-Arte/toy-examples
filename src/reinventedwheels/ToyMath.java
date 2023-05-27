29fa3e6a62ad3a7e197e63d88cc77462f4d095bc
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-27 16:46:31 -0400

    Failing random( ) is in unit interval test

--- src/reinventedwheels/ToyMath.java
+++ src/reinventedwheels/ToyMath.java
@@ -1,4 +1,25 @@
 package reinventedwheels;
 
 class ToyMath {
+
+    // TODO: Write tests for this
+    static double floor(double x) {
+        return Double.MAX_VALUE;
+    }
+
+    // TODO: Write tests for this
+    static double ceiling(double x) {
+        return Double.MIN_VALUE;
+    }
+
+    // TODO: Write tests for this
+    static double abs(double x) {
+        return Double.MIN_VALUE;
+    }
+
+    // TODO: Write tests for this
+    static double random() {
+        return Double.POSITIVE_INFINITY;
+    }
+
 }
--- test/reinventedwheels/ToyMathTest.java
+++ test/reinventedwheels/ToyMathTest.java
@@ -1,8 +1,27 @@
 package reinventedwheels;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToyMathTest {
+
+    @Test
+    void testRandomIsInUnitInterval() {
+        double runningTotal = 0.0;
+        double additionalIncrement = 0.1;
+        double adjusted = additionalIncrement;
+        while (adjusted < 100.0) {
+            double x = ToyMath.random();
+            String msg = "Pseudorandom number " + x
+                    + " should be at least 0.00 and less than 1.0";
+            assert x >= 0.0 : msg;
+            assert x < 1.0 : msg;
+            runningTotal += x;
+            adjusted += runningTotal + additionalIncrement;
+        }
+    }
 
 }
