76f811d69fba9d66ea28eba3300d0ee89e722c83
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-28 00:25:48 -0400

    Changed to public so as to play with these in a local REPL

--- src/reinventedwheels/ToyMath.java
+++ src/reinventedwheels/ToyMath.java
@@ -7,7 +7,7 @@ package reinventedwheels;
  * educational.
  * @author Alonso del Arte
  */
-class ToyMath {
+public class ToyMath {
 
     private static final long FLOATING_POINT_ALMOST_ONE_BIT_PATTERN
             = 0x3FEFFFFFFFFFFFFFL;
@@ -15,21 +15,21 @@ class ToyMath {
     private static long previousBitPattern = Double.doubleToLongBits(0.5);
 
     // TODO: Write tests for this
-    static double floor(double x) {
+    public static double floor(double x) {
         return Double.MAX_VALUE;
     }
 
     // TODO: Write tests for this
-    static double ceiling(double x) {
+    public static double ceiling(double x) {
         return Double.MIN_VALUE;
     }
 
     // TODO: Write tests for this
-    static double abs(double x) {
+    public static double abs(double x) {
         return Double.MIN_VALUE;
     }
 
-    static double random() {
+    public static double random() {
         long millis = System.currentTimeMillis();
         long bitPattern = (millis ^ (previousBitPattern * 37))
                 & FLOATING_POINT_ALMOST_ONE_BIT_PATTERN;
