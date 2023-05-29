6ecb99d79cfca92d24c109cc900d45759f55a65b
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-28 22:46:05 -0400

    Passing abs positive test

--- src/reinventedwheels/ToyMath.java
+++ src/reinventedwheels/ToyMath.java
@@ -40,7 +40,7 @@ public class ToyMath {
 
     // TODO: Write tests for this
     public static double abs(double x) {
-        if (Double.isInfinite(x) || Double.isNaN(x) || x == 0.0) {
+        if (Double.isInfinite(x) || Double.isNaN(x)) {
             return Double.MIN_VALUE;
         } else {
             return x;
