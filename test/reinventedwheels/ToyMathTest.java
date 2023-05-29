4863bb3719d120459fe16bd9a421a7b8f8147ec6
Alonso del Arte <alonso.delarte@gmail.com>
2023-05-28 22:45:13 -0400

    Failing abs positive zero test

--- test/reinventedwheels/ToyMathTest.java
+++ test/reinventedwheels/ToyMathTest.java
@@ -6,16 +6,29 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Tests of the ToyMath class.
+ * @author Alonso del Arte
+ */
 class ToyMathTest {
 
     @Test
     void testAbsPositive() {
         long millis = System.currentTimeMillis();
-        double expected = 1.0 / millis + (millis >> 32);
+        double expected = 2.5E16 / millis + 1.3E10 / millis;
         double actual = ToyMath.abs(expected);
         assertEquals(expected, actual);
     }
 
+    @Test
+    void testAbsPositiveZero() {
+        assertEquals(0.0, ToyMath.abs(0.0));
+    }
+
+    /**
+     * Test of the random function, of the ToyMath class. The return value of
+     * random() should be at least 0.0 but less than 1.0.
+     */
     @Test
     void testRandomIsInUnitInterval() {
         double runningTotal = 0.0;
@@ -33,6 +46,10 @@ class ToyMathTest {
         System.out.println("Pseudorandom numbers added up to " + runningTotal);
     }
 
+    /**
+     * Another test of the random function, of the ToyMath class. Called a
+     * hundred times, random() should give at least ninety distinct values.
+     */
     @Test
     void testRandomGivesEnoughDistinctValues() {
         int capacity = 100;
@@ -47,8 +64,17 @@ class ToyMathTest {
         assert actual >= expected : msg;
     }
 
+    /**
+     * Another test of the random function, of the ToyMath class. If the test
+     * divides the unit interval into sixteen "zones" and calls random() 256
+     * times, it should receive at least one number in each of the sixteen
+     * zones. However, this test does not measure the uniformity of the
+     * distribution, so, hypothetically, the test might receive one number in
+     * each of fifteen zones, and all the rest in a single zone, and it would
+     * still be considered passing.
+     */
     @Test
-    void testRandomDistributesWellAcrossUnitInterval() {
+    void testRandomDistributesWellEnoughAcrossUnitInterval() {
         int expected = 16;
         Set<Integer> numbers = new HashSet<>(expected);
         int attempts = expected * expected;
