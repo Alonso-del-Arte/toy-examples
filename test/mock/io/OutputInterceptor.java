a317d9172b0d4ae25646437d3dbc9f5012c8c878
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-02 20:24:47 -0400

    New package

---
+++ test/mock/io/OutputInterceptor.java
@@ -0,0 +1,27 @@
+package mock.io;
+
+import java.io.OutputStream;
+
+/**
+ * Helps intercept output to a <code>java.io.PrintStream</code>.
+ * @author Alonso del Arte
+ */
+public class OutputInterceptor extends OutputStream {
+
+    private final StringBuffer buffer
+            = new StringBuffer("SORRY, NOT IMPLEMENTED YET");
+
+    /**
+     *
+     * @return
+     */
+    public String getText() {
+        return this.buffer.toString();
+    }
+
+    @Override
+    public void write(int b) {
+        this.buffer.append((char) b);
+    }
+
+}
---
+++ test/mock/io/OutputInterceptorTest.java
@@ -0,0 +1,19 @@
+package mock.io;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import randomness.ExtendedRandom;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OutputInterceptorTest {
+
+    @Test
+    void testGetText() {
+        System.out.println("getText");
+        fail("Haven't written test yet");
+    }
+
+}
