62cdb26ffaccadda2923c819af8637baf84afa46
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-02 22:21:50 -0400

    Failing getText( ) test

--- test/mock/io/OutputInterceptorTest.java
+++ test/mock/io/OutputInterceptorTest.java
@@ -13,7 +13,23 @@ class OutputInterceptorTest {
     @Test
     void testGetText() {
         System.out.println("getText");
-        fail("Haven't written test yet");
+        int length = ExtendedRandom.nextInt(16) + 4;
+        String expected = ExtendedRandom.alphanumeric(length);
+        byte[] bytes = new byte[length];
+        char[] characters = expected.toCharArray();
+        for (int i = 0; i < length; i++) {
+            bytes[i] = (byte) characters[i];
+        }
+        OutputInterceptor interceptor = new OutputInterceptor();
+        try {
+            interceptor.write(bytes);
+            String actual = interceptor.getText();
+            assertEquals(actual, expected);
+        } catch (IOException ioe) {
+            String msg = "Encountered IOException trying to write \""
+                    + expected + "\", " + ioe.getMessage();
+            fail(msg);
+        }
     }
 
 }
