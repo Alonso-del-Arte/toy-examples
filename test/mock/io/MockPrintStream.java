0067f3dd06dc614bb6f6321431d0b7220b07edd4
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-08 01:38:12 -0400

    Deprecated

--- test/mock/io/MockPrintStream.java
+++ test/mock/io/MockPrintStream.java
@@ -6,8 +6,11 @@ import java.io.PrintStream;
  * Mocks a <code>java.io.PrintStream</code> for use with an {@link
  * OutputInterceptor}. Only a constructor is defined in this subclass of
  * <code>PrintStream</code>.
+ * @deprecated Just use <code>java.io.PrintStream</code>. This unnecessary
+ * subclass will be removed in a later commit.
  * @author Alonso del Arte
  */
+@Deprecated
 public class MockPrintStream extends PrintStream {
 
     public MockPrintStream(OutputInterceptor interceptor) {
--- test/mock/io/OutputInterceptor.java
+++ test/mock/io/OutputInterceptor.java
@@ -4,8 +4,11 @@ import java.io.OutputStream;
 
 /**
  * Helps intercept output to a <code>java.io.PrintStream</code>.
+ * @deprecated For most purposes, <code>java.io.ByteArrayOutputStream</code>
+ * should be sufficient.
  * @author Alonso del Arte
  */
+@Deprecated
 public class OutputInterceptor extends OutputStream {
 
     private final StringBuffer buffer = new StringBuffer();
