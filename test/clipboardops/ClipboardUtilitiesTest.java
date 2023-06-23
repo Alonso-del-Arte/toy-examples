3f0229cf028619eb79eeb912d4f0cac3ab59bfac
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-23 00:53:48 -0400

    Failing find primary flavor String test

--- test/clipboardops/ClipboardUtilitiesTest.java
+++ test/clipboardops/ClipboardUtilitiesTest.java
@@ -18,4 +18,14 @@ class ClipboardUtilitiesTest {
         assert flavor.isMimeTypeEqual(DataFlavor.imageFlavor) : msg;
     }
 
+    @Test
+    void testFindPrimaryFlavorString() {
+        DataFlavor[] flavors = {DataFlavor.stringFlavor,
+                DataFlavor.stringFlavor};
+        DataFlavor flavor = ClipboardUtilities.findPrimaryFlavor(flavors);
+        String msg = DataFlavor.stringFlavor.getMimeType()
+                + " should be MIME type of " + flavor;
+        assert flavor.isMimeTypeEqual(DataFlavor.stringFlavor) : msg;
+    }
+
 }
