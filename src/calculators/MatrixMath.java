c8121ea39e2b3894415d4e553e42b7097d0736d4
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-23 00:48:11 -0400

    Stubs

--- src/calculators/MatrixMath.java
+++ src/calculators/MatrixMath.java
@@ -1,4 +1,28 @@
 package calculators;
 
+import collections.mutable.Matrix;
+import collections.mutable.MatrixPath;
+
 public class MatrixMath {
+
+    // TODO: Write tests for this
+    public static Matrix sort(Matrix unsorted) {
+        return unsorted;
+    }
+
+    // TODO: Write tests for this
+    public static boolean contains(Matrix matrix, int element) {
+        return false;
+    }
+
+    // TODO: Write tests for this
+    public static int countIslands(Matrix area, int land) {
+        return -1;
+    }
+
+    // TODO: Write tests for this
+    public static MatrixPath findRatPath(Matrix area, int allowed) {
+        return new MatrixPath();
+    }
+
 }
--- test/calculators/MatrixMathTest.java
+++ test/calculators/MatrixMathTest.java
@@ -1,7 +1,23 @@
 package calculators;
 
+import collections.mutable.Matrix;
+import collections.mutable.MatrixPath;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixMathTest {
+
+    @Test
+    void testSort() {
+        System.out.println("sort");
+        fail("HAVEN'T WRITTEN TEST YET");
+    }
+
+    @Test
+    void testContains() {
+        System.out.println("contains");
+        fail("HAVEN'T WRITTEN TEST YET");
+    }
 
 }
