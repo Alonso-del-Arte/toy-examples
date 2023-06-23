65d47644afb54cfc336ab43b5cb2537f7952efd5
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-23 01:06:42 -0400

    Passing find primary flavor String test

--- src/clipboardops/ClipboardUtilities.java
+++ src/clipboardops/ClipboardUtilities.java
@@ -8,7 +8,12 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 public class ClipboardUtilities {
 
     public static DataFlavor findPrimaryFlavor(DataFlavor[] flavors) {
-        return DataFlavor.imageFlavor;
+        for (DataFlavor flavor : flavors) {
+            if (flavor.isMimeTypeEqual(DataFlavor.imageFlavor)) {
+                return flavor;
+            }
+        }
+        return flavors[0];
     }
 
 }
