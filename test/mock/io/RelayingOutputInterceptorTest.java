f85f8ab50c92290e9b8bd4d82e0cc38f613e6b04
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-26 15:12:34 -0400

    Failing relay test

--- test/mock/io/RelayingOutputInterceptorTest.java
+++ test/mock/io/RelayingOutputInterceptorTest.java
@@ -11,4 +11,21 @@ import randomness.ExtendedRandom;
 
 class RelayingOutputInterceptorTest {
 
+    @Test
+    void testWriteRelaysToRelayTarget() throws IOException {
+        System.out.println("write");
+        int length = ExtendedRandom.nextInt(32) + 8;
+        String expected = ExtendedRandom.alphanumeric(length);
+        byte[] bytes = new byte[length];
+        char[] characters = expected.toCharArray();
+        for (int i = 0; i < length; i++) {
+            bytes[i] = (byte) characters[i];
+        }
+        OutputStream relayTarget = new ByteArrayOutputStream();
+        OutputStream relayer = new RelayingOutputInterceptor(relayTarget);
+        relayer.write(bytes);String actual = relayTarget.toString()
+                .replace("\n", "").replace("\r", "");
+        assertEquals(expected, actual);
+    }
+
 }
