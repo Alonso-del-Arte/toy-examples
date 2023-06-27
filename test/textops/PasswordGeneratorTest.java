3a5f1c6b667cda514bfcd5be3af581d0fc9210e9
Alonso del Arte <alonso.delarte@gmail.com>
2023-06-27 15:40:32 -0400

    Failing a couple more tests

--- test/textops/PasswordGeneratorTest.java
+++ test/textops/PasswordGeneratorTest.java
@@ -1,6 +1,8 @@
 package textops;
 
 import java.security.SecureRandom;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,6 +24,24 @@ class PasswordGeneratorTest {
         String msg = "Generated password \"" + password
                 + "\" should match regular expression \"" + expression + "\"";
         assert opResult : msg;
+    }
+
+    @Test
+    void testGenerateAnew() {
+        String expression = "[a-zA-Z]{8}";
+        Pattern regex = Pattern.compile(expression);
+        SecureRandom random = new SecureRandom();
+        PasswordGenerator generator = new PasswordGenerator(random);
+        int expected = random.nextInt(16) + 4;
+        Set<String> passwords = new HashSet<>(expected);
+        for (int i = 0; i < expected; i++) {
+            passwords.add(generator.generate(regex));
+        }
+        String msg = "Calling generate() " + expected
+                + " times should have given " + expected
+                + " distinct passwords";
+        int actual = passwords.size();
+        assertEquals(expected, actual, msg);
     }
 
 }
