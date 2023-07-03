6c3e08d0ffc920b7593a26fff5238af05b2d3c62
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-03 15:10:39 -0400

    Failing constructor rejects negative length test

--- test/calculators/BinaryStringsCollectorTest.java
+++ test/calculators/BinaryStringsCollectorTest.java
@@ -3,6 +3,7 @@ package calculators;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -11,7 +12,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryStringsCollectorTest {
 
-    @BeforeAll
-    static void setUpClass() {}
+//    @BeforeAll
+//    static void setUpClass() {}
+
+    @Test
+    void testConstructorRejectsNegativeLength() {
+        Random random = new Random();
+        byte badLength = (byte) (-random.nextInt(128));
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            BinaryStringsCollector badCollector
+                    = new BinaryStringsCollector(badLength);
+            System.out.println("Should not have created " + badCollector
+                    + " with bad length " + badLength);
+        });
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        System.out.println("\"" + excMsg + "\"");
+    }
 
 }
