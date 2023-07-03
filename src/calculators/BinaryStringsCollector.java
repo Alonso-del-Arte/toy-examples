f822ea0ab138cf8b4a8e56e308b3521cdfa7520e
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-03 14:47:09 -0400

    New class

---
+++ src/calculators/BinaryStringsCollector.java
@@ -0,0 +1,20 @@
+package calculators;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BinaryStringsCollector {
+
+    // TODO: Write tests for this
+    public Set<String> give() {
+        Set<String> set = new HashSet<>();
+        set.add("SORRY, NOT IMPLEMENTED YET");
+        return set;
+    }
+
+    // TODO: Write tests for this
+    public BinaryStringsCollector(byte length) {
+        //
+    }
+
+}
---
+++ test/calculators/BinaryStringsCollectorTest.java
@@ -0,0 +1,17 @@
+package calculators;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryStringsCollectorTest {
+
+    @BeforeAll
+    static void setUpClass() {}
+
+}
