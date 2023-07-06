5228f27d7a29f4cbe52171bd8d63a681993cb9fb
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-06 18:47:09 -0400

    Refactored to use numeric traversals

--- src/calculators/BinaryStringsCollector.java
+++ src/calculators/BinaryStringsCollector.java
@@ -6,6 +6,9 @@ import java.util.Set;
 public class BinaryStringsCollector {
 
     private final byte len;
+
+    private final int max;
+
     private final Set<String> set = new HashSet<>();
 
     public Set<String> give() {
@@ -16,22 +19,19 @@ public class BinaryStringsCollector {
         return String.format("%1$" + this.len + "s", s).replace(' ', '0');
     }
 
-    private void addZero(String numStr) {
-        String doubled = numStr + '0';
-        if (doubled.length() <= this.len) {
-            this.set.add(leftZeroPad(doubled));
-            this.addZero(doubled);
-            this.addOne(numStr);
+    private void traverseOdd(int n) {
+        if (n < this.max) {
+            this.set.add(this.leftZeroPad(Integer.toString(n, 2)));
+            this.traverseEven(2 * n);
         }
     }
 
-    private void addOne(String numStr) {
-        String doubledPlusOne = numStr + '1';
-        if (doubledPlusOne.length() <= this.len) {
-            if (!doubledPlusOne.contains("11")) {
-                this.set.add(leftZeroPad(doubledPlusOne));
-                this.addZero(doubledPlusOne);
-            }
+    private void traverseEven(int n) {
+        if (n < this.max) {
+            this.set.add(this.leftZeroPad(Integer.toString(n, 2)));
+            int twice = 2 * n;
+            this.traverseEven(twice);
+            this.traverseOdd(twice + 1);
         }
     }
 
@@ -42,8 +42,10 @@ public class BinaryStringsCollector {
             throw new IllegalArgumentException(excMsg);
         }
         this.len = length;
+        this.max = 1 << this.len;
         if (this.len > 0) {
-            this.addZero("");
+            this.set.add(this.leftZeroPad("0"));
+            this.traverseOdd(1);
         }
     }
 
