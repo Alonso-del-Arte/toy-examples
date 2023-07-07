8853eba88ed34eb812c1075de8eab9470e1d3d69
Alonso del Arte <alonso.delarte@gmail.com>
2023-07-07 01:12:09 -0400

    New class

---
+++ src/calculators/TextCalculator.java
@@ -0,0 +1,25 @@
+package calculators;
+
+public class TextCalculator {
+
+    // TODO: Write tests for this
+    public static String padWithSpacesLeft(String s, int length) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static String padWithSpacesRight(String s, int length) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static String padLeft(String s, int length, char c) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public static String padRight(String s, int length, char c) {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+}
