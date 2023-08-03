cd812f4ecb8d4aedbf1fdddb53ca4944aa3876b6
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-03 02:04:05 -0400

    Failing reject size below minimum test

--- test/collections/caches/NRUCacheTest.java
+++ test/collections/caches/NRUCacheTest.java
@@ -7,19 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NRUCacheTest {
 
-//    @Test
-//    void testConstructorRejectsSizeBelowMinimum() {
-//        int badSize = NRUCache.MINIMUM_CAPACITY - 1;
-//        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
-//            NRUCacheImpl badCache = new NRUCacheImpl(badSize);
-//            System.out.println("Should not have been able to create "
-//                    + badCache + " of size " + badSize
-//                    + ", one less than minimum capacity");
-//        });
-//        String excMsg = t.getMessage();
-//        assert excMsg != null : "Message should not be null";
-//        System.out.println("\"" + excMsg + "\"");
-//    }
+    @Test
+    void testConstructorRejectsSizeBelowMinimum() {
+        int badSize = NRUCache.MINIMUM_CAPACITY - 1;
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            NRUCacheImpl badCache = new NRUCacheImpl(badSize);
+            System.out.println("Should not have been able to create "
+                    + badCache + " of size " + badSize
+                    + ", one less than minimum capacity");
+        });
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        System.out.println("\"" + excMsg + "\"");
+    }
 
     private static class NRUCacheImpl extends NRUCache<String, Pattern> {
 
