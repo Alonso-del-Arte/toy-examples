8d6b708d08b3442b14d518cb7de37b2f899af940
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-03 01:27:23 -0400

    Duplicated a couple of constants redundantly for now

--- src/collections/caches/NRUCache.java
+++ src/collections/caches/NRUCache.java
@@ -3,6 +3,19 @@ package collections.caches;
 public abstract class NRUCache<N, V> {
 
     /**
+     * The minimum capacity for a cache. Obviously this should not be a negative
+     * number, and zero doesn't make sense either. A value of 1 would be
+     * pointless, since the cache would be constantly pushing items out. So
+     * perhaps 2 is the smallest value that makes sense. But I think 4 is the
+     * smallest value likely to be used with any frequency. I don't think the
+     * cache should be too large, however, though I'm not providing a maximum
+     * capacity constant.
+     */
+    public static final int MINIMUM_CAPACITY = 4;
+
+    public static final int MAXIMUM_CAPACITY = 128;
+
+    /**
      * Creates a value for a given name. Ideally this function should only be
      * called by {@link #retrieve(java.lang.Object) retrieve()}.
      * @param name The name to create a value for. Once the value is in the
