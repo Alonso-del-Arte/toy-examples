da69a97c0ebd9b604e32d990b7f519001c1b3ce0
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-10 01:28:04 -0400

    Failing constructor rejects negative size test

--- test/collections/caches/CacheTest.java
+++ test/collections/caches/CacheTest.java
@@ -6,6 +6,8 @@ import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import static randomness.ExtendedRandom.nextInt;
+
 class CacheTest {
 
     static final String MIN_CAP_STR = Integer.toString(Cache.MINIMUM_CAPACITY);
@@ -30,6 +32,24 @@ class CacheTest {
             numStr = Integer.toString(badSize);
         } while (numStr.contains(MIN_CAP_STR) || numStr.contains(MAX_CAP_STR));
         return badSize;
+    }
+
+    @Test
+    void testConstructorRejectsNegativeSize() {
+        int badSize = CacheTest.vetBadSize(nextInt(-512) - 1, -1);
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            CacheImpl badCache = new CacheImpl(badSize);
+            System.out.println("Should not have been able to create "
+                    + badCache + " of size " + badSize);
+        });
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        assert !excMsg.isEmpty() : "Message should not be empty";
+        assert excMsg.contains(CacheTest.MIN_CAP_STR)
+                : CacheTest.CAPACITY_ASSERTION_MESSAGE;
+        assert excMsg.contains(CacheTest.MAX_CAP_STR)
+                : CacheTest.CAPACITY_ASSERTION_MESSAGE;
+        System.out.println("\"" + excMsg + "\"");
     }
 
     /**
