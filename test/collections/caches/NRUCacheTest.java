bdc8abe1bae0444644ef012c3961050fcdc5f236
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-10 00:48:40 -0400

    Failing constructor rejects size below minimum test

--- test/collections/caches/NRUCacheTest.java
+++ test/collections/caches/NRUCacheTest.java
@@ -27,6 +27,27 @@ class NRUCacheTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    void testConstructorRejectsSizeBelowMinimum() {
+        for (int i = 0; i < Cache.MINIMUM_CAPACITY; i++) {
+            final int badSize = i;
+            Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+                NRUCacheImpl badCache = new NRUCacheImpl(badSize);
+                System.out.println("Should not have been able to create "
+                        + badCache + " of size " + badSize
+                        + ", which is less than minimum capacity "
+                        + Cache.MINIMUM_CAPACITY);
+            });
+            String excMsg = t.getMessage();
+            assert excMsg != null : "Message should not be null";
+            assert excMsg.contains(CacheTest.MIN_CAP_STR)
+                    : CacheTest.CAPACITY_ASSERTION_MESSAGE;
+            assert excMsg.contains(CacheTest.MAX_CAP_STR)
+                    : CacheTest.CAPACITY_ASSERTION_MESSAGE;
+            System.out.println("\"" + excMsg + "\"");
+        }
+    }
+
     private static class NRUCacheImpl extends NRUCache<String, Pattern> {
 
         @Override
