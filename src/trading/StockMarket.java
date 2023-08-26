a6e53f47e270b5d0aefe4d6f7e7f5abe59376ed8
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-25 20:57:29 -0400

    Passing get random name test

--- src/trading/StockMarket.java
+++ src/trading/StockMarket.java
@@ -4,19 +4,8 @@ public class StockMarket {
 
     private final String fullName;
 
-    // TODO: Write tests for this
     public String getName() {
-        switch (this.fullName) {
-            case "New York Stock Exchange":
-                return "New York Stock Exchange";
-            case "NYSE Chicago":
-                return "NYSE Chicago";
-            case "National Association of Securities Dealers Automated Quotations":
-                return "National Association of Securities Dealers Automated Quotations";
-            default:
-                return "SORRY, NOT IMPLEMENTED YET";
-        }
-
+        return this.fullName;
     }
 
     // TODO: Write tests for this
