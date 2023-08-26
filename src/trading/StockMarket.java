f7318bcb8b7340f0a46dcc1226e4febd3d032ee5
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-25 20:09:46 -0400

    Passing getName( ) test

--- src/trading/StockMarket.java
+++ src/trading/StockMarket.java
@@ -2,9 +2,21 @@ package trading;
 
 public class StockMarket {
 
+    private final String fullName;
+
     // TODO: Write tests for this
     public String getName() {
-        return "SORRY, NOT IMPLEMENTED YET";
+        switch (this.fullName) {
+            case "New York Stock Exchange":
+                return "New York Stock Exchange";
+            case "NYSE Chicago":
+                return "NYSE Chicago";
+            case "National Association of Securities Dealers Automated Quotations":
+                return "National Association of Securities Dealers Automated Quotations";
+            default:
+                return "SORRY, NOT IMPLEMENTED YET";
+        }
+
     }
 
     // TODO: Write tests for this
@@ -12,8 +24,14 @@ public class StockMarket {
         return "SORRY, NOT IMPLEMENTED YET";
     }
 
+    // TODO: Write tests for this
+    public StockMarket(String name) {
+        this(name, "SNIY");
+    }
+
     public StockMarket(String name, String abbrev) {
         // TODO: Write tests for this
+        this.fullName = name;
     }
 
 }
