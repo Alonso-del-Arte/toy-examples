e10e86d0f7ad02c043ebe336a9c11a1a63a981dd
Alonso del Arte <alonso.delarte@gmail.com>
2023-08-25 21:25:43 -0400

    Passing get random abbreviation test

--- src/trading/StockMarket.java
+++ src/trading/StockMarket.java
@@ -8,18 +8,8 @@ public class StockMarket {
         return this.fullName;
     }
 
-    // TODO: Write tests for this
     public String getAbbreviation() {
-        switch (this.abbreviation) {
-            case "NYSE":
-                return "NYSE";
-            case "CHX":
-                return "CHX";
-            case "NASDAQ":
-                return "NASDAQ";
-            default:
-                return "SORRY, NOT IMPLEMENTED YET";
-        }
+        return this.abbreviation;
     }
 
     // TODO: Write tests for this
