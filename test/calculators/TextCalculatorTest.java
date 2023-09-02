192e4ee2ae4c78f130ce5ff96e82cc12827f9244
Alonso del Arte <alonso.delarte@gmail.com>
2023-09-01 21:59:02 -0400

    Failing nextObject(array) test

--- test/calculators/TextCalculatorTest.java
+++ test/calculators/TextCalculatorTest.java
@@ -3,6 +3,15 @@ package calculators;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import randomness.ExtendedRandom;
+
 class TextCalculatorTest {
+
+    private final static String[] ADJECTIVES = {"Equestrian", "Federal", "International", "Local", "National", "State"};
+
+    private static final String[] SINGULAR_NOUNS = {"Academy", "Bureau",
+            "Chamber", "Department"};
+
+    public static final String[] PLURAL_NOUNS = {"Investigations"};
 
 }
