66c945d81643d8e8617e57b210a2245a03d76f0a
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-05 01:54:18 -0400

    Failing isHighSurrogate( ) test

--- test/calculators/TextCalculatorTest.java
+++ test/calculators/TextCalculatorTest.java
@@ -7,11 +7,24 @@ import randomness.ExtendedRandom;
 
 class TextCalculatorTest {
 
-    private final static String[] ADJECTIVES = {"Equestrian", "Federal", "International", "Local", "National", "State"};
+    private final static String[] ADJECTIVES = {"Associated", "Ballistic",
+            "Consolidated", "Diversified", "Equestrian", "Federal", "Global",
+            "Historical", "International", "Local", "National", "State"};
 
     private static final String[] SINGULAR_NOUNS = {"Academy", "Bureau",
             "Chamber", "Department"};
 
-    public static final String[] PLURAL_NOUNS = {"Investigations"};
+    public static final String[] PLURAL_NOUNS = {"Armadillos", "Bullfrogs",
+            "Clerks", "Investigations", "Jokes", "Kangaroos", "Llamas"};
+
+    @Test
+    void testIsHighSurrogate() {
+        System.out.println("isHighSurrogate");
+        int span = 0xDC00 - 0xD800;
+        char ch = (char) (0xD800 + ExtendedRandom.nextInt(span));
+        String msg = "Character " + Character.getName(ch)
+                + " should be found to be a high surrogate";
+        assert TextCalculator.isHighSurrogate(ch) : msg;
+    }
 
 }
