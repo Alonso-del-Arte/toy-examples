d37f24b7363ebf5795a0b9e9f84c3e321d037e84
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-05 01:45:05 -0400

    More stubs

--- src/calculators/TextCalculator.java
+++ src/calculators/TextCalculator.java
@@ -3,6 +3,21 @@ package calculators;
 public class TextCalculator {
 
     // TODO: Write tests for this
+    public static boolean isHighSurrogate(char ch) {
+        return false;
+    }
+
+    // TODO: Write tests for this
+    public static boolean isLowSurrogate(char ch) {
+        return false;
+    }
+
+    // TODO: Write tests for this
+    public static boolean isOutsideBMP(String s) {
+        return false;
+    }
+
+    // TODO: Write tests for this
     public static String padWithSpacesLeft(String s, int length) {
         return "SORRY, NOT IMPLEMENTED YET";
     }
