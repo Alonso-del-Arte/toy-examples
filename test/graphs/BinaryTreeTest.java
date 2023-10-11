842aad7bc65a6df92fca8b865dff5f294578a06b
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-11 01:35:59 -0400

    Failing node's getElement( ) test

--- test/graphs/BinaryTreeTest.java
+++ test/graphs/BinaryTreeTest.java
@@ -1,7 +1,26 @@
 package graphs;
 
+import org.junit.jupiter.api.Test;
+import randomness.ExtendedRandom;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeTest {
+
+    @Test
+    void testNodeGetElement() {
+        System.out.println("getElement");
+        int length = ExtendedRandom.nextInt(16) + 4;
+        String expected = ExtendedRandom.alphanumeric(length);
+        BinaryTree.Node<String> node = new BinaryTree.Node<>(expected);
+        String actual = node.getElement();
+        assertEquals(expected, actual);
+    }
+
+    //    @Test
+    void testGetRoot() {fail("Haven't written test yet");
+        System.out.println("getRoot");
+        BinaryTree<Integer> tree;
+    }
 
 }
