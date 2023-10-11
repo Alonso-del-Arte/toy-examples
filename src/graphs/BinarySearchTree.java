2bfc0a031d4ee14b876d1712c4d4a25e7cfdcb0b
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-10 20:19:24 -0400

    New package

---
+++ src/graphs/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package graphs;
+
+public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
+
+    BinarySearchTree(E root) {
+        super(root);
+    }
+
+}
---
+++ src/graphs/BinaryTree.java
@@ -0,0 +1,23 @@
+package graphs;
+
+public class BinaryTree<E extends Comparable<E>> {
+
+    public BinaryTree(E root) {}
+
+    public static class Node<E> {
+
+        public Node(E element) {
+            //
+        }
+
+        Node(Node<E> ancestor, E element) {
+            //
+        }
+
+        private Node(Node<E> ancestor, E element, Node<E> leftChild) {
+            //
+        }
+
+    }
+
+}
