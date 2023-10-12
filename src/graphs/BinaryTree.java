d9252a7a0da35508892366a7872c1b14f0b1fa69
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-11 20:50:31 -0400

    Passing getRoot( ) test

--- src/graphs/BinaryTree.java
+++ src/graphs/BinaryTree.java
@@ -1,18 +1,48 @@
 package graphs;
 
+/**
+ * Represents a binary tree. This class is mutable, though the root element
+ * can't be changed after construction.
+ * @param <E> The type of the root, branches and leaves. Should implement the
+ *           <code>Comparable&lt;E&gt;</code> interface. For example,
+ *           <code>BigInteger</code>.
+ */
 public class BinaryTree<E extends Comparable<E>> {
 
-    // TODO: Write tests for this
+    private final Node<E> rootNode;
+
+    /**
+     * Gives the root node of this binary tree.
+     * @return A node containing the root element.
+     */
     public Node<E> getRoot() {
-        return null;
+        return this.rootNode;
     }
 
-    public BinaryTree(E root) {}
+    /**
+     * Sole constructor. Initializes a tree with only a root node.
+     * @param root The root element. This can't be changed after construction.
+     *             All other nodes can be changed.
+     */
+    public BinaryTree(E root) {
+        this.rootNode = new Node<>(root);
+    }
 
+    /**
+     * Represents a node in the tree. It may be the root node, a branch node or
+     * a leaf node.
+     * @param <E> The type of elements in the tree, must be the same as the one
+     *           the tree was declared to be of, though of course this is not
+     *           checked at runtime.
+     */
     public static class Node<E> {
 
         private final E element;
 
+        /**
+         * Gives this node's element.
+         * @return The element that was passed to the node constructor.
+         */
         public E getElement() {
             return this.element;
         }
