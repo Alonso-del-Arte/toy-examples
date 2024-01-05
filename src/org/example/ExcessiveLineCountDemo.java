ecb87a20fce9cbe0e3f9cbb596af46f4fcbedea2
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-05 00:04:58 -0500

    Increased line count without increasing functionality

--- src/org/example/ExcessiveLineCountDemo.java
+++ src/org/example/ExcessiveLineCountDemo.java
@@ -2,10 +2,19 @@ package org.example;
 
 public class ExcessiveLineCountDemo {
 
-    public static void main(String[] args) {
-        int stop = Integer.parseInt(args[0]);
-        for (int i = 1; i <= stop; i++) {
-            System.out.println(i);
+    public static void main(String[] args)
+    {
+        int stop
+                = Integer
+                        .parseInt(args[0]);
+        for (int i = 1;
+             i <= stop;
+             i++)
+        {
+            System
+                    .out
+                            .println(i);
         }
     }
+
 }
