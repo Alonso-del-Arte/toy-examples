70742723a98d4a057aacdd6ba57fb5e014c85d66
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-14 00:27:59 -0400

    Failing is not empty test

--- test/collections/mutable/HashSetTest.java
+++ test/collections/mutable/HashSetTest.java
@@ -1,6 +1,7 @@
 package collections.mutable;
 
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +10,18 @@ class HashSetTest {
 
     @Test
     void testIsEmpty() {
+        System.out.println("isEmpty");
         HashSet<ResultSet> set = new HashSet<>();
         assert set.isEmpty() : "Newly created set should be empty";
     }
+
+    @Test
+    void testIsNotEmpty() {
+        HashSet<LocalDateTime> set = new HashSet<>();
+        LocalDateTime dateTime = LocalDateTime.now();
+        set.add(dateTime);String msg = "After adding " + dateTime
+                + " to set of times, set should not be empty";
+        assert !set.isEmpty() : msg;
+    }
+
 }
