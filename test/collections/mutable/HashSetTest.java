c6386e3fcac8bf8b1bcb285904c7a75d127be833
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-13 23:58:06 -0400

    Failing isEmpty( ) test

--- test/collections/mutable/HashSetTest.java
+++ test/collections/mutable/HashSetTest.java
@@ -1,4 +1,15 @@
+package collections.mutable;
+
+import java.sql.ResultSet;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class HashSetTest {
-  
+
+    @Test
+    void testIsEmpty() {
+        HashSet<ResultSet> set = new HashSet<>();
+        assert set.isEmpty() : "Newly created set should be empty";
+    }
 }
