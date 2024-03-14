8f6c376efa472716b5bdb91209c8d959215e90d2
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-13 23:57:23 -0400

    New class, new test class

---
+++ src/collections/mutable/HashSet.java
@@ -0,0 +1,21 @@
+package collections.mutable;
+
+public class HashSet<E> {
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return Integer.MIN_VALUE;
+    }
+
+    public boolean add(E element) {
+        return false;
+    }
+
+    public HashSet() {
+        //
+    }
+
+}
