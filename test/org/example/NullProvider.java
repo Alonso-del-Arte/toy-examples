c7142a00782a9e8d16e48a44fe0124cbf7cfb0cc
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-15 01:17:18 -0400

    New class, new test class

---
+++ test/org/example/NullProvider.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class NullProvider {
+
+    public static Object provideNull() {
+        return "SORRY";
+    }
+
+}
---
+++ test/org/example/NullProviderTest.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NullProviderTest {
+}
