eed4159a6a63c17c5e66c13fdf3cf42f7ca0c893
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-15 01:20:21 -0400

    Failing provideNull( ) test

--- test/org/example/NullProviderTest.java
+++ test/org/example/NullProviderTest.java
@@ -4,4 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NullProviderTest {
+
+    @Test
+    void testProvideNull() {
+        System.out.println("provideNull");
+        Object actual = NullProvider.provideNull();
+        assertNull(actual);
+    }
+
 }
