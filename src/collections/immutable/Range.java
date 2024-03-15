3a65918e6e45cec9a6eb9af0cda8e646770eec51
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-15 01:38:06 -0400

    Passing not equals diff class test

--- src/collections/immutable/Range.java
+++ src/collections/immutable/Range.java
@@ -48,7 +48,7 @@ public class Range implements Iterable<Integer> {
 
     @Override
     public boolean equals(Object obj) {
-        return obj != null;
+        return obj instanceof Range;
     }
 
     @Override
