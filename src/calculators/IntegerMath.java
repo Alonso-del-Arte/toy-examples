70385e0dc4b4f3e4752727e57ea745b105fe983c
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-16 23:58:25 -0400

    Passing is not power of 2 test

--- src/calculators/IntegerMath.java
+++ src/calculators/IntegerMath.java
@@ -1,6 +1,7 @@
 package calculators;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -15,8 +16,14 @@ import randomness.ExtendedRandom;
  */
 public class IntegerMath {
 
+    private static final int[] POWERS_OF_TWO = {1, 2, 4, 8, 16, 32, 64,
+            128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
+            131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608,
+            16777216, 33554432, 67108864, 134217728, 268435456, 536870912,
+            1073741824};
+
     public static boolean isPowerOfTwo(int n) {
-        return n > 0;
+        return Arrays.binarySearch(POWERS_OF_TWO, n) > -1;
     }
 
     /**
