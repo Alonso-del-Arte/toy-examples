4a937502083718b49a29dbe3b302a5d6f0179d88
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-10 01:17:24 -0500

    Failing is empty at first test

--- test/collections/mutable/FrequencyCounterTest.java
+++ test/collections/mutable/FrequencyCounterTest.java
@@ -5,4 +5,10 @@ import org.junit.jupiter.api.Test;
 
 class FrequencyCounterTest {
 
+    @Test
+    void testIsEmptyAtFirst() {
+        FrequencyCounter<String> instance = new FrequencyCounter<>();
+        assert instance.isEmpty() : "Collection should be empty at first";
+    }
+
 }
