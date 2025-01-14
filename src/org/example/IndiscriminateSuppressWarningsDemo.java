9d34ccb36331ad9daf425f232430ed469c7b0abd
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-13 23:36:29 -0500

    New demo

---
+++ src/org/example/IndiscriminateSuppressWarningsDemo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+@SuppressWarnings("all")
+public class IndiscriminateSuppressWarningsDemo {
+
+    private void actuateB(double x) {
+        //
+    }
+
+    void actuateA(int n) {
+        //
+    }
+
+    public void actuate(String param) {
+        //
+    }
+
+}
