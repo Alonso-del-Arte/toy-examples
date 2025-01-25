b8886c7c8e9c8ab9444f9e55560c32c76fce8a40
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-24 23:48:44 -0500

    Failing word count 0 for empty String test

--- test/textops/WordCounterTest.java
+++ test/textops/WordCounterTest.java
@@ -1,7 +1,17 @@
 package textops;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class WordCounterTest {
+
+    @Test
+    void testWordCountZeroForEmptyString() {
+        WordCounter instance = new WordCounter("");
+        int expected = 0;
+        int actual = instance.wordCount();
+        String message = "Word count for empty String should be 0";
+        assertEquals(expected, actual, message);
+    }
 
 }
