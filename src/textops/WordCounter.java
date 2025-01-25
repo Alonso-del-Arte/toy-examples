2037c629c4e15959f492fda5c25f6783917b0ffa
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-24 23:45:10 -0500

    wordCount( ) stub

--- src/textops/WordCounter.java
+++ src/textops/WordCounter.java
@@ -2,8 +2,13 @@ package textops;
 
 public class WordCounter {
 
+    // TODO: Write tests for this
+    public int wordCount() {
+        return -1;
+    }
+
     public WordCounter(String s) {
         //
     }
-    
+
 }
