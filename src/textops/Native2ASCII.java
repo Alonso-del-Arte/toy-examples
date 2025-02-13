8325d61e53f55e255c660acce423978aeb66a465
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-12 22:43:12 -0500

    New class

---
+++ src/textops/Native2ASCII.java
@@ -0,0 +1,13 @@
+package textops;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Native2ASCII {
+
+    public static void main(String[] args) {
+        //
+    }
+
+}
---
+++ test/textops/Native2ASCIITest.java
@@ -0,0 +1,17 @@
+package textops;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Native2ASCIITest {
+
+    @Test
+    void placeholder() {
+        fail("HAVEN'T WRITTEN TESTS YET");
+    }
+
+}
