7c127a6df047e4bad650a4aa07fe7a6444b6c98a
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-12 22:39:41 -0500

    Preparing to test chooseBMPBlock( )

--- test/randomness/ExtendedRandomTest.java
+++ test/randomness/ExtendedRandomTest.java
@@ -3,6 +3,7 @@ package randomness;
 import fractions.Fraction;
 
 import java.awt.Color;
+import static java.lang.Character.UnicodeBlock.*;
 import java.math.BigInteger;
 import java.sql.Statement;
 import java.time.LocalDateTime;
@@ -21,6 +22,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExtendedRandomTest {
 
     public static final Random RANDOM = new Random(-System.currentTimeMillis());
+
+    /**
+     * A selection of Unicode blocks from the Basic Multilingual Plane. Since
+     * this project uses Java 8 rather than the latest LTS, the following
+     * Unicode blocks are missing:
+     * <ul>
+     *     <li>Syriac Supplement</li>
+     *     <li>Arabic Extended B</li>
+     *     <li>Cyrillic Extended C</li>
+     *     <li>Georgian Extended</li>
+     *     <li>Myanmar Extended B</li>
+     *     <li>Latin Extended E</li>
+     *     <li>Cherokee Supplement</li>
+     * </ul>
+     */
+    private static final Character.UnicodeBlock[] UNICODE_BLOCKS
+            = {BASIC_LATIN, LATIN_1_SUPPLEMENT, LATIN_EXTENDED_A,
+            LATIN_EXTENDED_B, IPA_EXTENSIONS, GREEK, CYRILLIC,
+            CYRILLIC_SUPPLEMENTARY, ARMENIAN, HEBREW, ARABIC, SYRIAC,
+            ARABIC_SUPPLEMENT, THAANA, NKO, SAMARITAN, MANDAIC,
+            ARABIC_EXTENDED_A, DEVANAGARI, BENGALI, GURMUKHI, GUJARATI, ORIYA,
+            TAMIL, TELUGU, KANNADA, MALAYALAM, SINHALA, THAI, LAO, TIBETAN,
+            MYANMAR, GEORGIAN, HANGUL_JAMO, ETHIOPIC, ETHIOPIC_SUPPLEMENT,
+            CHEROKEE, UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS, OGHAM, RUNIC,
+            TAGALOG, HANUNOO, BUHID, TAGBANWA, KHMER, MONGOLIAN,
+            UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS_EXTENDED, LIMBU, TAI_LE,
+            NEW_TAI_LUE, BUGINESE, TAI_THAM, BALINESE, SUNDANESE, BATAK,
+            LEPCHA, OL_CHIKI, SUNDANESE_SUPPLEMENT, VEDIC_EXTENSIONS,
+            LATIN_EXTENDED_ADDITIONAL, GREEK_EXTENDED, GLAGOLITIC,
+            LATIN_EXTENDED_C, COPTIC, GEORGIAN_SUPPLEMENT, TIFINAGH,
+            ETHIOPIC_EXTENDED, CYRILLIC_EXTENDED_A, HIRAGANA, KATAKANA,
+            BOPOMOFO, KANBUN, BOPOMOFO_EXTENDED,
+            CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A, CJK_UNIFIED_IDEOGRAPHS,
+            YI_SYLLABLES, LISU, VAI, CYRILLIC_EXTENDED_B, BAMUM,
+            LATIN_EXTENDED_D, SYLOTI_NAGRI, PHAGS_PA, SAURASHTRA,
+            DEVANAGARI_EXTENDED, KAYAH_LI, REJANG, HANGUL_JAMO_EXTENDED_A,
+            JAVANESE, CHAM, MYANMAR_EXTENDED_A, TAI_VIET,
+            MEETEI_MAYEK_EXTENSIONS, ETHIOPIC_EXTENDED_A, MEETEI_MAYEK,
+            HANGUL_SYLLABLES, HANGUL_JAMO_EXTENDED_B};
+
 
     @Test
     void testNextInt() {
