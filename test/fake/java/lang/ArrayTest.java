66eeb4b63cbac3b290c50ab45f5c631c18756d67
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-13 11:41:52 -0400

    Failing length test

--- test/fake/java/lang/ArrayTest.java
+++ test/fake/java/lang/ArrayTest.java
@@ -1,7 +1,25 @@
 package fake.java.lang;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static randomness.ExtendedRandom.*;
 
 class ArrayTest {
+
+    @Test
+    void testLength() {
+        System.out.println("length");
+        int expected = nextInt(16) + 4;
+        BigInteger[] bigInts = new BigInteger[expected];
+        for (int i = 0; i < expected; i++) {
+            bigInts[i] = nextBigInt(32 + i);
+        }
+        Array<BigInteger> instance = new Array<>(bigInts);
+        int actual = instance.length;
+        assertEquals(expected, actual);
+    }
 
 }
