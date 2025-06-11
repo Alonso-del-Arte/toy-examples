2a4b42b294a0ef43207c75cc21ad5f293909c161
Alonso del Arte <alonso.delarte@gmail.com>
2025-06-10 23:08:58 -0400

    Failing toString( ) test

--- test/fake/java/lang/ArrayTest.java
+++ test/fake/java/lang/ArrayTest.java
@@ -1,6 +1,7 @@
 package fake.java.lang;
 
 import java.math.BigInteger;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -19,6 +20,27 @@ class ArrayTest {
         }
         Array<BigInteger> instance = new Array<>(bigInts);
         int actual = instance.length;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testToString() {
+        System.out.println("toString");
+        int capacity = nextInt(8) + 2;
+        LocalDate[] dates = new LocalDate[capacity];
+        LocalDate today = LocalDate.now();
+        StringBuilder s = new StringBuilder("[");
+        for (int i = 0; i < capacity; i++) {
+            int daysToAdd = nextInt(365) + i;
+            LocalDate date = today.plusDays(daysToAdd);
+            dates[i] = date;
+            s.append(date);
+            s.append(',');
+        }
+        s.append(']');
+        Array<LocalDate> instance = new Array<>(dates);
+        String expected = s.toString().replace(",]", "]");
+        String actual = instance.toString().replace(" ", "");
         assertEquals(expected, actual);
     }
 
