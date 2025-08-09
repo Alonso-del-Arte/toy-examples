23f7efd68e88465d54e6f0e3cbb094d9ecef3413
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-08 22:47:13 -0400

    New class and test class

---
+++ src/randomness/RandomBabbleOutputStream.java
@@ -0,0 +1,15 @@
+package randomness;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RandomBabbleOutputStream extends InputStream {
+
+    // TODO: Write tests for this
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+
+}
---
+++ test/randomness/RandomBabbleOutputStreamTest.java
@@ -0,0 +1,17 @@
+package randomness;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomBabbleOutputStreamTest {
+
+    @Test
+    void placeholder() {
+        fail("HAVEN'T WRITTEN ANY TESTS YET");
+    }
+
+}
