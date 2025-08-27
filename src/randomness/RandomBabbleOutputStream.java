60fdd5317a86f5a22141b8571a8ce53a908806f2
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-26 23:06:46 -0400

    Passing read( ) test

--- src/randomness/RandomBabbleOutputStream.java
+++ src/randomness/RandomBabbleOutputStream.java
@@ -3,13 +3,16 @@ package randomness;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Random;
 
 public class RandomBabbleOutputStream extends InputStream {
+
+    public static final Random RANDOM = new Random(-System.currentTimeMillis());
 
     // TODO: Write tests for this
     @Override
     public int read() throws IOException {
-        return 0;
+        return RANDOM.nextInt(95) + 32;
     }
 
 }
