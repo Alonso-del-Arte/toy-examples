2b4e54fdef3d8fba17b3942865eebba2d1ad14d0
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-10 23:35:26 -0400

    Failing toString( ) test

--- test/graphics/DownsampledColorTest.java
+++ test/graphics/DownsampledColorTest.java
@@ -5,6 +5,25 @@ import java.awt.Color;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import static randomness.ExtendedRandom.nextInt;
+
 class DownsampledColorTest {
+
+    @Test
+    void testToString() {
+        System.out.println("toString");
+        int n = nextInt(256);
+        int red = (n & 48) >> 4;
+        int green = (n & 12) >> 2;
+        int blue = n & 3;
+        int alpha = (n & 192) >> 6;
+        byte b = (byte) n;
+        DownsampledColor instance = new DownsampledColor(b);
+        String expected = "[r = " + red + ", g = " + green + ", b = " + blue
+                + ", a = " + alpha + "]";
+        String actual = instance.toString();
+        String message = "Color from byte " + b;
+        assertEquals(expected, actual, message);
+    }
 
 }
