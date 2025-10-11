b192db90124f15b7387aa17871032f18667ece6a
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-10 23:39:31 -0400

    Passing toString( ) test

--- src/graphics/DownsampledColor.java
+++ src/graphics/DownsampledColor.java
@@ -4,12 +4,25 @@ import java.awt.Color;
 
 public class DownsampledColor {
 
+    private final byte colorByte;
+
+    @Override
+    public String toString() {
+        int red = (this.colorByte & 48) >> 4;
+        int green = (this.colorByte & 12) >> 2;
+        int blue = this.colorByte & 3;
+        int alpha = (this.colorByte & 192) >> 6;
+        return "[r = " + red + ", g = " + green + ", b = " + blue + ", a = "
+                + alpha + "]";
+    }
+
     DownsampledColor(byte b) {
-        // TODO: Write tests for this
+        this.colorByte = b;
     }
 
     public DownsampledColor(Color color) {
         // TODO: Write tests for this
+        this.colorByte = 0;
     }
 
 }
