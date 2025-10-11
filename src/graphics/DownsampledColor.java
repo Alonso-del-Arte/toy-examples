8722fbb4e62763381243af8f3b64c9c882391bbe
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-10 23:16:23 -0400

    New package

---
+++ src/graphics/DownsampledColor.java
@@ -0,0 +1,15 @@
+package graphics;
+
+import java.awt.Color;
+
+public class DownsampledColor {
+
+    DownsampledColor(byte b) {
+        // TODO: Write tests for this
+    }
+
+    public DownsampledColor(Color color) {
+        // TODO: Write tests for this
+    }
+
+}
---
+++ test/graphics/DownsampledColorTest.java
@@ -0,0 +1,10 @@
+package graphics;
+
+import java.awt.Color;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class DownsampledColorTest {
+
+}
