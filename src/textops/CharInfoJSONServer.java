298e9c0f1941809b2bc9a06ee1b620655f4626ea
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-16 22:53:19 -0400

    New class

---
+++ src/textops/CharInfoJSONServer.java
@@ -0,0 +1,21 @@
+package textops;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Currency;
+import java.util.Locale;
+
+public class CharInfoJSONServer {
+}
---
+++ test/textops/CharInfoJSONServerTest.java
@@ -0,0 +1,28 @@
+package textops;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharInfoJSONServerTest {
+
+}
