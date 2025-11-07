0f7993856f0c28269c37e5d95bdf9b1934026af8
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-07 00:03:43 -0500

    Passing is not outside BMP test

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -61,7 +61,7 @@ public class TextCalculator {
         if (s == null) {
             throw new NullPointerException("Parameter s should not be null");
         }
-        return true;
+        return false;
     }
 
     // TODO: Write tests for this
