589348388f7f2fd36d796a683ec09b249fb2f07e
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-15 23:58:45 -0500

    Failing padWithSpacesRight( ) rejects negative length test

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -228,4 +228,23 @@ class TextCalculatorTest {
         assert excMsg.contains(numStr) : containsMsg;
     }
 
+    @Test
+    void testPadWithSpacesRightRejectsNegativeLength() {
+        int badLength = ExtendedRandom.nextInt() | Integer.MIN_VALUE;
+        String message = "padWithSpacesLeft() should reject length "
+                + badLength;
+        Throwable t = assertThrows(IllegalArgumentException.class, () -> {
+            String badResult = TextCalculator.padWithSpacesRight(message,
+                    badLength);
+            System.out.println(message + ", not given result " + badResult);
+        }, message);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        String numStr = Integer.toString(badLength);
+        String containsMsg = "Exception message should contain \"" + numStr
+                + "\"";
+        assert excMsg.contains(numStr) : containsMsg;
+    }
+
 }
