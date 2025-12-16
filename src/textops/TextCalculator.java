54066741f7a78b39a479ef2e0f6c36127fc415fc
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-16 00:09:00 -0500

    Passing padLeft( ) rejects negative length test

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -112,6 +112,10 @@ public class TextCalculator {
 
     // TODO: Write tests for this
     public static String padLeft(String s, int length, char c) {
+        if (length < 0) {
+            String excMsg = "Length " + length + " is not valid";
+            throw new IllegalArgumentException(excMsg);
+        }
         return "SORRY, NOT IMPLEMENTED YET";
     }
 
