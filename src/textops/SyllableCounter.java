46024bd0a605019d83695d018fb93440932f664b
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-24 16:07:10 -0500

    New class

---
+++ src/textops/SyllableCounter.java
@@ -0,0 +1,21 @@
+package textops;
+
+public class SyllableCounter {
+
+    private final String text;
+
+    // TODO: Write tests for this
+    public String getText() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    public int syllableCount() {
+        return -1;
+    }
+
+    public SyllableCounter(String s) {
+        this.text = "SORRY, NOT IMPLEMENTED YET";
+    }
+
+}
---
+++ test/textops/SyllableCounterTest.java
@@ -0,0 +1,13 @@
+package textops;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class SyllableCounterTest {
+
+    @Test
+    void placeholder() {
+        fail("WRITE TESTS");
+    }
+
+}
