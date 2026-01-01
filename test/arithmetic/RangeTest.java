54e8a1cb0e771096f7a5abc2cf01c08f9e563f1b
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-01 16:01:29 -0500

    New class

---
+++ test/arithmetic/RangeTest.java
@@ -0,0 +1,11 @@
+package arithmetic;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangeTest {
+
+    public static final Random RANDOM = new Random(~System.currentTimeMillis());
+
+}
