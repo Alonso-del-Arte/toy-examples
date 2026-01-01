cf9e9935f12c7b359cf6ca26219d72434a768708
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-01 16:10:08 -0500

    Getter stubs

--- src/arithmetic/Range.java
+++ src/arithmetic/Range.java
@@ -2,6 +2,21 @@ package arithmetic;
 
 public class Range {
 
+    // TODO: Write tests for this
+    public int getStart() {
+        return Integer.MAX_VALUE;
+    }
+
+    // TODO: Write tests for this
+    public int getEnd() {
+        return Integer.MIN_VALUE;
+    }
+
+    // TODO: Write tests for this
+    public int getStep() {
+        return 0;
+    }
+
     public Range(int start, int end) {
         // TODO: Write tests for this
     }
