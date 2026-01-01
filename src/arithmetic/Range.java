f30d208e71bbd4ec5bc895a7f6f7abdb1a705570
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-01 16:06:46 -0500

    Constructor stubs

--- src/arithmetic/Range.java
+++ src/arithmetic/Range.java
@@ -1,4 +1,13 @@
 package arithmetic;
 
 public class Range {
+
+    public Range(int start, int end) {
+        // TODO: Write tests for this
+    }
+
+    public Range(int start, int end, int step) {
+        // TODO: Write tests for this
+    }
+
 }
