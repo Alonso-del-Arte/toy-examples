e6d7d57fb73cbba48dc7b0406e42120960efc6f2
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-09 22:50:38 -0500

    Passing referential equality test

--- src/arithmetic/Range.java
+++ src/arithmetic/Range.java
@@ -55,7 +55,7 @@ public class Range implements Iterable<Integer> {
     // TODO: Write tests for this
     @Override
     public boolean equals(Object obj) {
-        return true;
+        return obj != null;
     }
 
     // TODO: Write tests for this
