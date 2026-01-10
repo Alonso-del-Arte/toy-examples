34890b5039d6506fca9be86c588f06bab01c33e1
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-09 23:32:00 -0500

    Failing not equals diff start test, both implicit step 1

--- test/arithmetic/RangeTest.java
+++ test/arithmetic/RangeTest.java
@@ -301,6 +301,17 @@ class RangeTest {
         }
     }
 
+    @Test
+    void testNotEqualsDiffStartBothImplicitStep1() {
+        int startA = RANDOM.nextInt(256) - 128;
+        int startB = startA + RANDOM.nextInt(256) + 4;
+        int end = startB + RANDOM.nextInt(128) + 2;
+        Range rangeA = new Range(startA, end);
+        Range rangeB = new Range(startB, end);
+        String message = rangeA + " should not equal " + rangeB;
+        assertNotEquals(rangeA, rangeB, message);
+    }
+
     // TODO: Test hashCode()
 
     // TODO: Test constructor rejects wrong direction (negative) step
