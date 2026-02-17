d4a1a47a786245f55821ac2b9d55bd89af67bb39
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-17 00:02:17 -0500

    Passing test for original prefix constant

--- src/retail/books/ISBN13.java
+++ src/retail/books/ISBN13.java
@@ -2,7 +2,6 @@ package retail.books;
 
 public class ISBN13 {
 
-    // TODO: Write a test for this
-    public static final short ORIGINAL_ISBN_13_PREFIX = -977;
+    public static final short ORIGINAL_ISBN_13_PREFIX = 978;
 
 }
