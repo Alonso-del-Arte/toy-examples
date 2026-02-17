4e984d908fdd41cb5515bf4230c67cd677250b1c
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-17 00:06:05 -0500

    Passing test for second prefix constant

--- src/retail/books/ISBN13.java
+++ src/retail/books/ISBN13.java
@@ -4,7 +4,6 @@ public class ISBN13 {
 
     public static final short ORIGINAL_ISBN_13_PREFIX = 978;
 
-    // TODO: Write tests for this
-    public static final short SECOND_ISBN_13_PREFIX = -978;
+    public static final short SECOND_ISBN_13_PREFIX = 979;
 
 }
