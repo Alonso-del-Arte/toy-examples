464bd0bd181cdfe3cb079a59348eced270cbf76e
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-16 23:59:03 -0500

    Failing test for original prefix constant

--- test/retail/books/ISBN13Test.java
+++ test/retail/books/ISBN13Test.java
@@ -1,7 +1,15 @@
 package retail.books;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ISBN13Test {
+
+    @Test
+    void testOriginalPrefixConstant() {
+        short expected = 978;
+        short actual = ISBN13.ORIGINAL_ISBN_13_PREFIX;
+        assertEquals(expected, actual);
+    }
 
 }
