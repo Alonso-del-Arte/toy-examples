4e52129d3d80a072ca73c08a7354a46fbb55eb0a
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-20 00:22:33 -0500

    Passing all check digit tests

--- src/retail/books/ISBN10.java
+++ src/retail/books/ISBN10.java
@@ -2,9 +2,16 @@ package retail.books;
 
 public class ISBN10 extends ISBN {
 
-    // TODO: Write tests for this
     static byte checkDigit(int num) {
-        return 0;
+        int sum = 0;
+        int curr = num;
+        for (int i = 9; i > 0; i--) {
+            int digit = curr % 10;
+            int weighted = digit * i;
+            sum += weighted;
+            curr /= 10;
+        }
+        return (byte) (sum % 11);
     }
 
     // TODO: Write tests for this
