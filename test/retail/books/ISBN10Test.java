1f99c5bfbd891465fee253da53343618eae8b942
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-19 23:33:07 -0500

    Preparing to write tests

--- test/retail/books/ISBN10Test.java
+++ test/retail/books/ISBN10Test.java
@@ -2,6 +2,22 @@ package retail.books;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static retail.books.ISBNTest.RANDOM;
+
 class ISBN10Test {
+
+    private static byte reckonCheckDigit(int digits) {
+        int sum = 0;
+        int curr = digits;
+        for (int i = 9; i > 0; i--) {
+            int digit = curr % 10;
+            int weighted = digit * i;
+            sum += weighted;
+            curr /= 10;
+        }
+        return (byte) sum;
+    }
+
+//    private static long chooseDigits(byte checkDigit)
 
 }
