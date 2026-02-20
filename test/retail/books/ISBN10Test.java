1af4a6006671b5bc4467298a60efe7fa33de7e3a
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-20 00:15:35 -0500

    Also failing check digit X test

--- test/retail/books/ISBN10Test.java
+++ test/retail/books/ISBN10Test.java
@@ -119,4 +119,13 @@ class ISBN10Test {
         assertEquals(expected, actual, message);
     }
 
+    @Test
+    void testCheckDigitX() {
+        byte expected = 10;
+        int num = chooseDigits(expected);
+        byte actual = ISBN10.checkDigit(num);
+        String message = "Getting check digit for " + num;
+        assertEquals(expected, actual, message);
+    }
+
 }
