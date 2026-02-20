ef737b378886959844ed8b1acb53d9a6983a786d
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-20 00:49:32 -0500

    Javadoc for checkDigit( )

--- src/retail/books/ISBN10.java
+++ src/retail/books/ISBN10.java
@@ -2,6 +2,22 @@ package retail.books;
 
 public class ISBN10 extends ISBN {
 
+    /**
+     * Reckons the check digit according to the ISBN-10 formula. Note that this
+     * formula is different from the ISBN-13 formula.
+     * @param num The number without the check digit. Ought to be at least
+     *            0-000-00000 and at most 99999-99-99, but this is not checked
+     *            and behavior for inputs outside that range is not documented.
+     *            For the example, we'll use 93-081-4885.
+     * @return The check digit. Provided {@code num} is in the appropriate
+     * range, this function will return at least 0 and at most 10, which is
+     * represented X. In the example, this would be 10. Thus the correctly
+     * formatted ISBN-10 would be, assuming I have placed the dashes correctly,
+     * 93-081-4885-X, which appears to be an unassigned ISBN as of February 19,
+     * 2026. If that number is indeed unassigned, it may have already been
+     * purchased long ago, but most likely it would be assigned ISBN-13
+     * 978-93-081-4885-0 upon registration.
+     */
     static byte checkDigit(int num) {
         int sum = 0;
         int curr = num;
