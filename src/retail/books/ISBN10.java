21d25365b63c73fd11a5caa6f916f393bae42286
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-19 23:26:04 -0500

    Decided 32-bit integers are enough for ISBN-10

--- src/retail/books/ISBN10.java
+++ src/retail/books/ISBN10.java
@@ -3,10 +3,10 @@ package retail.books;
 public class ISBN10 extends ISBN {
 
     // TODO: Write tests for this
-    static byte checkDigit(long num) {
+    static byte checkDigit(int num) {
         return 12;
     }
-    
+
     // TODO: Write tests for this
     static boolean isValid(String s) {
         return false;
