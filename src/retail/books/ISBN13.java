6fd1774bb2203e86a051601c2ae00f3f19322731
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-20 22:24:39 -0500

    Passing all check digits

--- src/retail/books/ISBN13.java
+++ src/retail/books/ISBN13.java
@@ -6,9 +6,21 @@ public class ISBN13 extends ISBN {
 
     public static final short SECOND_ISBN_13_PREFIX = 979;
 
-    // TODO: Write tests for this
     static byte checkDigit(long num) {
-        return 0;
+        int sum = 0;
+        long curr = num;
+        for (int i = 12; i > 0; i--) {
+            long digit = curr % 10;
+            int weight = (i % 2 == 0) ? 3 : 1;
+            long weighted = digit * weight;
+            sum += weighted;
+            curr /= 10;
+        }
+        int remainder = sum % 10;
+        if (remainder == 0) {
+            return 0;
+        }
+        return (byte) (10 - remainder);
     }
 
     // TODO: Write tests for this
