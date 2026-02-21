f77c20ac853d7d7c0f12df4e4cacb10a9603b857
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-20 22:10:32 -0500

    Failing test for check digit 1

--- test/retail/books/ISBN13Test.java
+++ test/retail/books/ISBN13Test.java
@@ -60,4 +60,13 @@ class ISBN13Test {
         assertEquals(expected, actual, message);
     }
 
+    @Test
+    void testCheckDigitOne() {
+        byte expected = 1;
+        long num = chooseDigits(expected);
+        byte actual = ISBN13.checkDigit(num);
+        String message = "Getting check digit for " + num;
+        assertEquals(expected, actual, message);
+    }
+
 }
