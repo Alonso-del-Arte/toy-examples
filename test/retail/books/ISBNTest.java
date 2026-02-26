6ed952d634faf97c6d53b1acb8319ce45b29bcdf
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-25 23:18:10 -0500

    Oops, should've corrected that in an earlier commit

--- test/retail/books/ISBNTest.java
+++ test/retail/books/ISBNTest.java
@@ -13,12 +13,12 @@ class ISBNTest {
     void testToString() {
         System.out.println("toString");
         final long num = 978_0_00000_000L + RANDOM.nextInt(10_00000_000);
-        ISBN isbn = new ISBN(num, "0") {};
+        fail("REWRITE THIS TEST");
+//        ISBN isbn = new ISBN(num, "0") {};
         StringBuilder intermediate = new StringBuilder(Long.toString(num));
         intermediate.insert(9, '-');
         intermediate.insert(4, '-');
         intermediate.insert(3, '-');
-        fail("REWRITE THIS TEST");
 //        String expected = intermediate.toString() + '-' + isbn.getCheckDigit();
 //        String actual = isbn.toString();
 //        assertEquals(expected, actual);
