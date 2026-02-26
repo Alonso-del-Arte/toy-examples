f0eae172f499f853477fb2e44fa6d8d1d675d019
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-25 23:52:14 -0500

    Failing toString( ) test

--- test/arithmetic/PercentageTest.java
+++ test/arithmetic/PercentageTest.java
@@ -1,7 +1,19 @@
 package arithmetic;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class PercentageTest {
+
+    @Test
+    void testToString() {
+        System.out.println("toString");
+        for (int i = 0; i < 101; i++) {
+            Percentage instance = new Percentage(i);
+            String expected = i + "%";
+            String actual = instance.toString();
+            assertEquals(expected, actual);
+        }
+    }
 
 }
