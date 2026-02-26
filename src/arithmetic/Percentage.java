0608d3e48f73000e0440195c7ab6ee82be1c421c
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-25 23:39:47 -0500

    Constructor stubs

--- src/arithmetic/Percentage.java
+++ src/arithmetic/Percentage.java
@@ -1,4 +1,16 @@
 package arithmetic;
 
+import java.math.BigDecimal;
+
 public class Percentage {
+
+    // TODO: Write tests for this
+    public Percentage(long value) {}
+
+    // TODO: Write tests for this
+    public Percentage(double value) {}
+
+    // TODO: Write tests for this
+    public Percentage(BigDecimal value) {}
+
 }
