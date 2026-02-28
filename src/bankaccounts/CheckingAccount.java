686965d71d34852c9671c8cdbea72db7b9287f9e
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-27 23:50:25 -0500

    Preparing to delete OverdraftTransferFee

--- src/bankaccounts/CheckingAccount.java
+++ src/bankaccounts/CheckingAccount.java
@@ -1,7 +1,6 @@
 package bankaccounts;
 
 import bankaccounts.transactions.Deposit;
-import bankaccounts.transactions.fees.OverdraftTransferFee;
 import bankaccounts.transactions.Withdrawal;
 import currency.CurrencyAmount;
 import entities.Entity;
@@ -21,31 +20,32 @@ public class CheckingAccount extends BankAccount {
 
     @Override
     public boolean processWithdrawal(Withdrawal withdrawal) {
-        if (this.accountHistory.contains(withdrawal)) {return false;}else{
-            CurrencyAmount projectedBalance = this.accountBalance.plus(withdrawal.getTransactionAmount());
-            if (projectedBalance.compareTo(BankAccount.INITIALIZATION_ACCOUNT_BALANCE) < 0) {
-                if (this.assocSav != null) {
-                    LocalDateTime transferDateTime = withdrawal.getTransactionDate().minusMinutes(2);
-                    Withdrawal transferWithdrawal = new Withdrawal(projectedBalance, transferDateTime);
-                    if (this.assocSav.processWithdrawal(transferWithdrawal)) {
-                        OverdraftTransferFee transferFee = new OverdraftTransferFee(transferDateTime);
-                        this.assocSav.processFee(transferFee);
-                        transferDateTime = transferDateTime.plusMinutes(1);
-                        CurrencyAmount deficitCompensation = projectedBalance.negate();
-                        Deposit transferDeposit = new Deposit(deficitCompensation, transferDateTime);
-                        this.processDeposit(transferDeposit);
-                        projectedBalance = BankAccount.INITIALIZATION_ACCOUNT_BALANCE;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return false;
-                }
-            }
-            this.accountBalance = projectedBalance;
-            this.accountHistory.add(withdrawal);
-            return true;
-        }
+        return true;
+//        if (this.accountHistory.contains(withdrawal)) {return false;}else{
+//            CurrencyAmount projectedBalance = this.accountBalance.plus(withdrawal.getTransactionAmount());
+//            if (projectedBalance.compareTo(BankAccount.INITIALIZATION_ACCOUNT_BALANCE) < 0) {
+//                if (this.assocSav != null) {
+//                    LocalDateTime transferDateTime = withdrawal.getTransactionDate().minusMinutes(2);
+//                    Withdrawal transferWithdrawal = new Withdrawal(projectedBalance, transferDateTime);
+//                    if (this.assocSav.processWithdrawal(transferWithdrawal)) {
+//                        OverdraftTransferFee transferFee = new OverdraftTransferFee(transferDateTime);
+//                        this.assocSav.processFee(transferFee);
+//                        transferDateTime = transferDateTime.plusMinutes(1);
+//                        CurrencyAmount deficitCompensation = projectedBalance.negate();
+//                        Deposit transferDeposit = new Deposit(deficitCompensation, transferDateTime);
+//                        this.processDeposit(transferDeposit);
+//                        projectedBalance = BankAccount.INITIALIZATION_ACCOUNT_BALANCE;
+//                    } else {
+//                        return false;
+//                    }
+//                } else {
+//                    return false;
+//                }
+//            }
+//            this.accountBalance = projectedBalance;
+//            this.accountHistory.add(withdrawal);
+//            return true;
+//        }
     }
 
     public CheckingAccount(Entity primary, Deposit initialDeposit) {
