022ee63cb5c24b39deee72528e3eb22d890671bc
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-06 00:54:06 -0400

    Failing first getMessage( ) test

--- test/retail/books/CheckDigitExceptionTest.java
+++ test/retail/books/CheckDigitExceptionTest.java
@@ -1,7 +1,24 @@
 package retail.books;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static randomness.ExtendedRandom.alphanumeric;
+import static randomness.ExtendedRandom.nextInt;
 
 class CheckDigitExceptionTest {
+
+    public static final int DEFAULT_TEST_MESSAGE_LENGTH = 20;
+
+    @Test
+    void testGetMessage3ParamConstructorA() {
+        byte badCheckDigit = (byte) nextInt();
+        int num = nextInt();
+        String expected = alphanumeric(DEFAULT_TEST_MESSAGE_LENGTH);
+        Throwable instance = new CheckDigitException(badCheckDigit, num,
+                expected);
+        String actual = instance.getMessage();
+        assertEquals(expected, actual);
+    }
 
 }
