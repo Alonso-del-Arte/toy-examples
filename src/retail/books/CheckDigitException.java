f388a3910050c942cd5d209c4f916671869cf5ae
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-06 00:42:22 -0400

    Constructor stubs

--- src/retail/books/CheckDigitException.java
+++ src/retail/books/CheckDigitException.java
@@ -1,4 +1,23 @@
 package retail.books;
 
 public class CheckDigitException extends RuntimeException {
+
+    // TODO: Write tests for this
+    public CheckDigitException(long num) {}
+
+    // TODO: Write tests for this
+    public CheckDigitException(String numStr) {}
+
+    // TODO: Write tests for this
+    public CheckDigitException(byte badCheckDigit, long num) {}
+
+    // TODO: Write tests for this
+    public CheckDigitException(byte badCheckDigit, String numStr) {}
+
+    // TODO: Write tests for this
+    public CheckDigitException(byte badCheckDigit, long num, String message) {}
+
+    // TODO: Write tests for this
+    public CheckDigitException(byte badCheckDigit, String numStr, String message) {}
+
 }
