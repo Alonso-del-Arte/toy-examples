36e01fd839c96f5bd873b69d04e2952f86d67f9a
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-16 23:39:07 -0400

    compareTo( ) stub

--- src/numerics/UUID.java
+++ src/numerics/UUID.java
@@ -1,9 +1,15 @@
 package numerics;
 
-public class UUID {
+public class UUID implements Comparable<UUID> {
+
+    // TODO: Write tests for this
+    @Override
+    public int compareTo(UUID o) {
+        return 0;
+    }
 
     public UUID(long highBits, long lowBits) {
         //
     }
-    
+
 }
