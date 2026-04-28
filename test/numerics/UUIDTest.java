a27da1992cca34c6be4cf3084a4a445d438dd00f
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-27 20:09:38 -0400

    Failing toString( ) test for null UUID

--- test/numerics/UUIDTest.java
+++ test/numerics/UUIDTest.java
@@ -8,4 +8,12 @@ import static textops.TextCalculator.padLeft;
 
 class UUIDTest {
 
+    @Test
+    void testToStringNullUUID() {
+        UUID instance = new UUID(0L, 0L);
+        String expected = "00000000-0000-0000-0000-000000000000";
+        String actual = instance.toString();
+        assertEquals(expected, actual);
+    }
+
 }
