95d0ec67ee57f22b93e82d589b89c965dc712046
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-05 23:38:46 -0400

    New class

---
+++ src/viewers/KeystrokeReporter.java
@@ -0,0 +1,58 @@
+package viewers;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+public final class KeystrokeReporter extends JFrame implements ActionListener,
+        KeyListener {
+
+    private static final int DEFAULT_COLUMNS = 20;
+
+    private JLabel keyId = new JLabel();
+
+    private JLabel modifiers = new JLabel();
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        //
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        this.keyId.setText("A key has been released");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+        this.keyId.setText("A key has been pressed");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent event) {
+        this.keyId.setText("A key has been typed");
+    }
+
+    public KeystrokeReporter() {
+        this.add(this.keyId, BorderLayout.PAGE_START);
+        this.add(this.modifiers, BorderLayout.PAGE_END);
+    }
+
+    public static void main(String[] args) {
+        KeystrokeReporter frame = new KeystrokeReporter();
+        Dimension preferredSize = new Dimension(400, 400);
+        frame.setPreferredSize(preferredSize);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.addKeyListener(frame);
+        frame.setVisible(true);
+    }
+
+}
