d2228502f5caf9c6eac02a0e64429107b4ecb413
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-06 00:31:27 -0400

    New test class

---
+++ test/viewers/KeystrokeReporterTest.java
@@ -0,0 +1,20 @@
+package viewers;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class KeystrokeReporterTest {
+
+}
