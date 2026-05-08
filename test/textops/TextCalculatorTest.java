bec73a62dd0c760b5914d7dc77c5ac9b8fecb078
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-08 16:41:59 -0400

    Failing blockRange( ) test for Latin-1 Supplement

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -33,6 +33,15 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeLatin1Supplement() {
+        Character.UnicodeBlock block
+                = Character.UnicodeBlock.LATIN_1_SUPPLEMENT;
+        Range expected = new Range(0x00A0, 0x00FF);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
