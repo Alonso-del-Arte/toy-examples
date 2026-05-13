31996efcdb307adc9c0e2d7c3784d4853756b689
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-13 01:05:51 -0400

    Failing blockRange( ) test for Spacing Modifiers

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -69,6 +69,15 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeSpacingModifiers() {
+        Character.UnicodeBlock block
+                = Character.UnicodeBlock.SPACING_MODIFIER_LETTERS;
+        Range expected = new Range(0x02B0, 0x02FF);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
