72ab1677cc3eda508a311c7adc453fc234abf5c6
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-14 15:32:38 -0400

    Failing blockRange( ) test for Arabic Extended A

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -192,6 +192,14 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeArabicExtendedA() {
+        Character.UnicodeBlock block = Character.UnicodeBlock.ARABIC_EXTENDED_A;
+        Range expected = new Range(0x08A0, 0x08FF);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
