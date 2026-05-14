a7aac1f4230b735cf741cf8d270db9ac5b46408a
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-13 23:48:47 -0400

    Failing blockRange( ) test for Cyrillic

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -95,6 +95,14 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeCyrillic() {
+        Character.UnicodeBlock block = Character.UnicodeBlock.CYRILLIC;
+        Range expected = new Range(0x0400, 0x04FF);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
