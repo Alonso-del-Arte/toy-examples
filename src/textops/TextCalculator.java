26a3046fe44807842ed471bc59fea9cd0210dcc2
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-14 15:24:59 -0400

    Passing blockRange( ) test for Mandaic

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -40,6 +40,7 @@ public class TextCalculator {
         BLOCK_RANGES.put(Character.UnicodeBlock.NKO, new Range(1984, 2047));
         BLOCK_RANGES.put(Character.UnicodeBlock.SAMARITAN,
                 new Range(2048, 2110));
+        BLOCK_RANGES.put(Character.UnicodeBlock.MANDAIC, new Range(2112, 2142));
     }
 
     // TODO: Write tests for this
