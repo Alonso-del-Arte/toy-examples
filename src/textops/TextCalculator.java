8827b2dceb0c6b04dc9a8bcc7f3ebc05217d2377
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-14 14:54:49 -0400

    Passing blockRange( ) test for Thaana

--- src/textops/TextCalculator.java
+++ src/textops/TextCalculator.java
@@ -36,6 +36,7 @@ public class TextCalculator {
         BLOCK_RANGES.put(Character.UnicodeBlock.SYRIAC, new Range(1792, 1871));
         BLOCK_RANGES.put(Character.UnicodeBlock.ARABIC_SUPPLEMENT,
                 new Range(1872, 1919));
+        BLOCK_RANGES.put(Character.UnicodeBlock.THAANA, new Range(1920, 1969));
     }
 
     // TODO: Write tests for this
