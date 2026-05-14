70d595a3262b80706e79d45764b13b13c561366b
Alonso del Arte <alonso.delarte@gmail.com>
2026-05-14 15:26:45 -0400

    Failing blockRange( ) test for Syriac supplement

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -184,6 +184,14 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeSyriacSupplement() {
+        Character.UnicodeBlock block = Character.UnicodeBlock.SYRIAC_SUPPLEMENT;
+        Range expected = new Range(0x0860, 0x086A);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
