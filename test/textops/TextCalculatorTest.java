aefb33bd0c5568c1916acf46558fc0189898b1c1
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-02 23:59:54 -0400

    Failing block range test for Lao

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -296,6 +296,14 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeLao() {
+        Character.UnicodeBlock block = Character.UnicodeBlock.LAO;
+        Range expected = new Range(0x0E81, 0x0EDF);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
