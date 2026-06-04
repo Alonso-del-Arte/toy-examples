663aafd3e8ac3892d58b58f2fc109c83235907ce
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-03 23:59:45 -0400

    Failing block range test for Georgian

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -320,6 +320,14 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeGeorgian() {
+        Character.UnicodeBlock block = Character.UnicodeBlock.MYANMAR;
+        Range expected = new Range(0x10A0, 0x10FF);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
