f9a33f28185b68515cd66375c2604ce91271ac3e
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-03 23:54:52 -0400

    Failing block range test for Tibetan

--- test/textops/TextCalculatorTest.java
+++ test/textops/TextCalculatorTest.java
@@ -304,6 +304,14 @@ class TextCalculatorTest {
     }
 
     @Test
+    void testBlockRangeTibetan() {
+        Character.UnicodeBlock block = Character.UnicodeBlock.TIBETAN;
+        Range expected = new Range(0x0F00, 0x0FDA);
+        Range actual = TextCalculator.blockRange(block);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void testIsHighSurrogate() {
         System.out.println("isHighSurrogate");
         int span = 0xDC00 - 0xD800;
