49859eb0ba28e254be6c622a60ee5cb551195b39
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-22 18:33:04 -0400

    Passing is of type RANDOM Version 4 test

--- src/numerics/UUIDType.java
+++ src/numerics/UUIDType.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 public enum UUIDType {
 
     UNKNOWN((UUID) -> false), MAC((UUID) -> false), SECURITY((UUID) -> false),
-    MD5((UUID) -> false), RANDOM((UUID) -> false), SHA1((UUID) -> false),
+    MD5((UUID) -> false), RANDOM((UUID) -> true), SHA1((UUID) -> false),
     MAC_SORTABLE((UUID) -> false), RANDOM_SORTABLE((UUID) -> false),
     CUSTOM((UUID) -> false);
 
