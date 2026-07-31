c3c04db6223174320f5fe861f131e6913ae5a04e
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-31 19:57:59 -0400

    Refactored magic constants out

--- src/numerics/UUIDType.java
+++ src/numerics/UUIDType.java
@@ -7,13 +7,25 @@ public enum UUIDType {
     UNKNOWN((UUID) -> false), MAC((UUID) -> false), SECURITY((UUID) -> false),
     MD5((UUID) -> false),
 
-    RANDOM((UUID uuid) -> (uuid.getHighBits() & 61440) == 16384),
+    RANDOM((UUID uuid) ->
+            (uuid.getHighBits() & Constants.VERSION_MASK)
+                    == Constants.VERSION_4_BIT),
 
     SHA1((UUID) -> false),
     MAC_SORTABLE((UUID) -> false), RANDOM_SORTABLE((UUID) -> false),
     CUSTOM((UUID) -> false);
 
     private final Predicate<UUID> checker;
+
+    private static class Constants {
+
+        private static final int VERSION_MASK = 61440;
+
+        private static final int VERSION_1_BIT = 4096;
+
+        private static final int VERSION_4_BIT = 16384;
+
+    }
 
     // TODO: Write tests for this
     public boolean isOfType(UUID uuid) {
