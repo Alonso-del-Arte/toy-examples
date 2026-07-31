a827bc938d511ae24d97bd2021b5a0e3dcc74c12
Alonso del Arte <alonso.delarte@gmail.com>
2026-07-31 12:51:38 -0400

    Failing test that Versions 0 to 3 UUIDs are not Version 4

--- test/numerics/UUIDTypeTest.java
+++ test/numerics/UUIDTypeTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 
 public class UUIDTypeTest {
 
+    private static final long HIGH_BITS_VERSION_INCREMENT = 4096L;
+
+    private static final long HIGH_BITS_VERSION_THRESHOLD = 65536L;
+
+    private static final long HIGH_BITS_VERSION_MASK_OUT = -61441L;
+
     private static final long DCE_VARIANT_MASK = -4611686018427387905L;
 
     private static final Random RANDOM = new Random(DCE_VARIANT_MASK);
@@ -16,6 +22,10 @@ public class UUIDTypeTest {
                 + (RANDOM.nextInt(32768) << 16) + 16384 + RANDOM.nextInt(2048);
         long lowBits = (RANDOM.nextLong() | Long.MIN_VALUE) & DCE_VARIANT_MASK;
         return new UUID(highBits, lowBits);
+    }
+
+    private static long version0HighBits() {
+        return RANDOM.nextLong() & HIGH_BITS_VERSION_MASK_OUT;
     }
 
     // TODO: Test MAC
@@ -31,7 +41,19 @@ public class UUIDTypeTest {
         assert UUIDType.RANDOM.isOfType(uuid) : msg;
     }
 
-    // TODO: Test is not RANDOM (version 4)
+    @Test
+    void testVersions0To3AreNotVersion4() {
+        long propHighBits = version0HighBits();
+        long lowBits = RANDOM.nextLong();
+        long threshold = 4 * HIGH_BITS_VERSION_INCREMENT;
+        for (long versionBits = 0; versionBits < threshold;
+             versionBits += HIGH_BITS_VERSION_INCREMENT) {
+            long highBits = propHighBits + versionBits;
+            UUID uuid = new UUID(highBits, lowBits);
+            String msg = uuid + " should not be Version 4";
+            assert !UUIDType.RANDOM.isOfType(uuid) : msg;
+        }
+    }
 
     // TODO: Test SHA1
 
