0ad81838eb070a33bce7ba2fbbd3d323c5944104
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-03 14:25:27 -0400

    New enumerated type

---
+++ src/numerics/UUIDVariant.java
@@ -0,0 +1,10 @@
+package numerics;
+
+import java.util.function.Predicate;
+
+public enum UUIDVariant {
+
+    NCS_BACKWARD_COMPATIBLE, DISTRIBUTED_COMPUTING_ENVIRONMENT, MICROSOFT_GUID,
+    RESERVED_FOR_FUTURE_USE, UNKNOWN
+
+}
