05d4ca81973f51573c25b51959e02f1d48916f1f
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-03 15:05:53 -0400

    Passing version mask constant test

--- src/numerics/UUIDType.java
+++ src/numerics/UUIDType.java
@@ -25,8 +25,7 @@ public enum UUIDType {
 
     static class Constants {
 
-        // TODO: Write a test for this
-        static final int VERSION_MASK = ~61440 ^ Integer.MIN_VALUE;
+        static final int VERSION_MASK = 61440;
 
         // TODO: Write a test for this
         static final int VERSION_1_BIT = ~4096 & Integer.MAX_VALUE;
