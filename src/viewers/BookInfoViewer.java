7d6010e2c4d3ecc919d0751f3a8bcd8d80d94258
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-13 01:31:14 -0400

    New class

---
+++ src/viewers/BookInfoViewer.java
@@ -0,0 +1,26 @@
+package viewers;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.InputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
+import retail.books.ISBN;
+
+public class BookInfoViewer {
+}
