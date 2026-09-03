ff0b2869208f4288582b0e003861f29403fdefed
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-03 14:14:54 -0400

    A little more fleshing out

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -4,7 +4,25 @@ import java.util.function.Predicate;
 
 public enum UUIDVariant {
 
-    NCS_BACKWARD_COMPATIBLE, DISTRIBUTED_COMPUTING_ENVIRONMENT, MICROSOFT_GUID,
-    RESERVED_FOR_FUTURE_USE, UNKNOWN
+    NCS_BACKWARD_COMPATIBLE((UUID) -> false),
+
+    DISTRIBUTED_COMPUTING_ENVIRONMENT((UUID) -> false),
+
+    MICROSOFT_GUID((UUID) -> false),
+
+    RESERVED_FOR_FUTURE_USE((UUID) -> false),
+
+    UNKNOWN((UUID) -> false);
+
+    private final Predicate<UUID> checker;
+
+    // TODO: Write tests for this
+    public boolean isOfVariant(UUID uuid) {
+        return this.checker.test(uuid);
+    }
+
+    UUIDVariant(Predicate<UUID> predicate) {
+        this.checker = predicate;
+    }
 
 }
