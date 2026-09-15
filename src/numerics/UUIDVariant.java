c4c49d2b6356886cd04516046777fa39a0b90eb2
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-15 00:59:35 -0400

    Passing high four bits mask constant test

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -18,8 +18,7 @@ public enum UUIDVariant {
 
     static class Constants {
         
-        // TODO: Write a test for this
-        static final long HIGH_FOUR_BITS_MASK = 1152921504606846975L;
+        static final long HIGH_FOUR_BITS_MASK = -1152921504606846976L;
 
     }
 
