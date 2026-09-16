3db8a1f28ed18c8ab3a9a0cdbe322b902c546fcf
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-15 23:08:27 -0400

    Passing is NCS backward compatible test

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public enum UUIDVariant {
 
-    NCS_BACKWARD_COMPATIBLE((UUID) -> false),
+    NCS_BACKWARD_COMPATIBLE((UUID) -> true),
 
     DISTRIBUTED_COMPUTING_ENVIRONMENT((UUID) -> false),
 
