3de1795232d8ce7c972f7597854d4d75f542a099
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-15 22:44:49 -0400

    Back to passing variant increment constant test

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -20,7 +20,7 @@ public enum UUIDVariant {
         
         static final long HIGH_FOUR_BITS_MASK = -1152921504606846976L;
 
-        static final long VARIANT_INCREMENT = -1L << 60;
+        static final long VARIANT_INCREMENT = 1152921504606846976L;
 
     }
 
