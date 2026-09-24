3b7521ad8211bfcfa99615ea9d68af12780dc04e
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-24 00:50:49 -0400

    Passing DCE not GUID D level test but failing DCE not GUID C level test

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -6,7 +6,9 @@ public enum UUIDVariant {
 
     NCS_BACKWARD_COMPATIBLE((UUID uuid) -> uuid.getLowBits() > -1),
 
-    DISTRIBUTED_COMPUTING_ENVIRONMENT((UUID uuid) -> uuid.getLowBits() < 0),
+    DISTRIBUTED_COMPUTING_ENVIRONMENT(
+            (UUID uuid) -> uuid.getLowBits() < Constants.MICROSOFT_GUID_D_LEVEL
+    ),
 
     MICROSOFT_GUID((UUID) -> false),
 
