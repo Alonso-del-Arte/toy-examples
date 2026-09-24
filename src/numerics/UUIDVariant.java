1dea2a4c14a7130a0c3b37e826ffdac9bdef1229
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-24 14:44:51 -0400

    Back to passing GUID D not DCE test but still failing GUID C not DCE test

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -7,8 +7,7 @@ public enum UUIDVariant {
     NCS_BACKWARD_COMPATIBLE((UUID uuid) -> uuid.getLowBits() > -1),
 
     DISTRIBUTED_COMPUTING_ENVIRONMENT(
-            (UUID uuid) -> uuid.getLowBits()
-                    < (-2 * Constants.VARIANT_INCREMENT)
+            (UUID uuid) -> uuid.getLowBits() < Constants.MICROSOFT_GUID_D_LEVEL
     ),
 
     MICROSOFT_GUID((UUID) -> false),
