2ffa329fe346eaf6cba1756273bb10db33a7f17e
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-23 23:12:02 -0400

    Passing NCS backward compatible is not DCE 1.1 test

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -6,7 +6,7 @@ public enum UUIDVariant {
 
     NCS_BACKWARD_COMPATIBLE((UUID uuid) -> uuid.getLowBits() > -1L),
 
-    DISTRIBUTED_COMPUTING_ENVIRONMENT((UUID) -> true),
+    DISTRIBUTED_COMPUTING_ENVIRONMENT((UUID uuid) -> uuid.getLowBits() < 0L),
 
     MICROSOFT_GUID((UUID) -> false),
 
