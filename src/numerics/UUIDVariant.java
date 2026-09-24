76810e2750cd27417f18340be0775a16124ff548
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-23 23:29:12 -0400

    Refactored NCS backward compatible checker

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public enum UUIDVariant {
 
-    NCS_BACKWARD_COMPATIBLE((UUID uuid) -> uuid.getLowBits() > -1L),
+    NCS_BACKWARD_COMPATIBLE((UUID uuid) -> uuid.getLowBits() > -1),
 
     DISTRIBUTED_COMPUTING_ENVIRONMENT((UUID uuid) -> uuid.getLowBits() < 0L),
 
