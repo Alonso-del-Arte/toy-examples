85a04fe18287791ec2541f88f22e9bad96806a82
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-23 23:58:35 -0400

    Renamed a constant

--- src/numerics/UUIDVariant.java
+++ src/numerics/UUIDVariant.java
@@ -23,7 +23,7 @@ public enum UUIDVariant {
         static final long VARIANT_INCREMENT = 1152921504606846976L;
 
         // TODO: Write a test for this
-        static final long MICROSOFT_GUID_C = -3L;
+        static final long MICROSOFT_GUID_C_LEVEL = -3L;
 
     }
 
