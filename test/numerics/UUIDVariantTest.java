a4c440703b86a6aaee6becd63fe771721d2f5224
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-23 23:48:13 -0400

    Failing Microsoft GUID C is not DCE test

--- test/numerics/UUIDVariantTest.java
+++ test/numerics/UUIDVariantTest.java
@@ -95,4 +95,15 @@ class UUIDVariantTest {
         }
     }
 
+    @Test
+    void testMicrosoftGUID_C_IsNotDCE() {
+        long highBits = chooseVersion4BitOrVersion7Bits();
+        long lowBits = ((RANDOM.nextLong() >>> 4) | Long.MIN_VALUE)
+                + (VAR_INCR << 2);
+        UUID uuid = new UUID(highBits, lowBits);
+        String msg = "UUID " + uuid + " should not be DCE 1.1";
+        assert !UUIDVariant.DISTRIBUTED_COMPUTING_ENVIRONMENT.isOfVariant(uuid)
+                : msg;
+    }
+
 }
